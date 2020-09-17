439e6bfd7e30be563ec3f347e8462aa6e0f34f69
tanhao1410 <2528865013@qq.com>
2020-09-18 07:10:54 +0800

    每日三题

--- 202009/day0918/day0918.go
+++ 202009/day0918/day0918.go
@@ -7,8 +7,108 @@ import (
 )
 
 func main() {
-	nums := []int{2, 1, 1, 3}
-	fmt.Println(permuteUnique(nums))
+	nums := []int{1, 2, 3, 4, 5, 6}
+	//fmt.Println(permuteUnique(nums))
+	head := createListByNums(nums)
+
+	//reorderList(head)
+	//findMiddleNode(head)
+	tree := sortedListToBST(head)
+	fmt.Println(tree)
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//有序表转化成二叉搜索树
+func sortedListToBST(head *ListNode) *TreeNode {
+	//想法：找到中间的数，作为根节点。切割成两半。左边，右边同样的流程进行递归操作
+	if head == nil {
+		return nil
+	}
+	if head.Next == nil {
+		return &TreeNode{head.Val, nil, nil}
+	}
+	middle := findMiddleNode(head)
+	root := &TreeNode{middle.Val, sortedListToBST(head), sortedListToBST(middle.Next)}
+	return root
+}
+
+//找list的中间节点，并切割
+func findMiddleNode(head *ListNode) *ListNode {
+	//快慢指针
+	//一个节点的时候不用切割
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	first, second, pre := head, head, head
+	for first != nil {
+		first = first.Next
+		if first == nil {
+			pre.Next = nil
+			return second
+		}
+		pre = second
+		first, second = first.Next, second.Next
+	}
+	pre.Next = nil
+	return second
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func createListByNums(nums []int) *ListNode {
+
+	var head *ListNode = nil
+	for i := len(nums) - 1; i >= 0; i-- {
+		node := &ListNode{nums[i], head}
+		head = node
+	}
+	return head
+}
+
+//重排链表，不能只交换值。
+//L0→Ln→L1→Ln-1→L2→Ln-2→…
+func reorderList(head *ListNode) {
+	if head == nil {
+		return
+	}
+	//思路：第一个后面是最后一个，第二个后面是倒数第二个。。。。
+	//先计算总数，把后面的几个拿出来，保存起来，然后拼接
+	nodes := []*ListNode{}
+	//求长度
+	listSize := 0
+	for p := head; p != nil; p = p.Next {
+		listSize++
+	}
+	//从len/2 -1的位置开始截取，截取的第一个的前一个，应该要指向nil
+	start := (listSize+1)/2 - 1
+	prev := head
+	for p := head; p != nil; p = p.Next {
+
+		if start >= listSize {
+			nodes = append(nodes, p)
+		}
+		if start == listSize-1 {
+			prev = p
+		}
+		start++
+	}
+	prev.Next = nil
+
+	//开始拼接
+	for p, i := head, len(nodes)-1; i >= 0; p = p.Next.Next {
+		p.Next, nodes[i].Next = nodes[i], p.Next
+		i--
+	}
+
+	fmt.Println("...")
 }
 
 //全排列2

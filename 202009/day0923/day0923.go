fc5ba1d8a8e4aeb702128205f986694a333bca6c
tanhao1410 <2528865013@qq.com>
2020-09-23 06:47:39 +0800

    二叉树中的链表-时间超时。。。

--- 202009/day0923/day0923.go
+++ 202009/day0923/day0923.go
@@ -4,6 +4,41 @@ func main() {
 
 }
 
+func isSubPath2(head *ListNode, root *TreeNode) bool {
+	return isSubPathFunc2(head, head, root)
+}
+
+func isSubPathFunc2(primitive, head *ListNode, root *TreeNode) bool {
+	//从root开始，找和head相等的，然后，递归调用，root向后走，head 向后走，
+	if head == nil {
+		return true
+	}
+	if root == nil {
+		return false
+	}
+
+	//每一次断了后，需要重新从头结点开始比较，而不能拿剩余的结点去比较了。
+	if head.Val == root.Val {
+		return isSubPathFunc2(primitive, head.Next, root.Left) ||
+			isSubPathFunc2(primitive, head.Next, root.Right)
+	}
+
+	//下一个结点不相等的话，新链表从头开始比较的时候，跳过了该节点。
+	if head == primitive {
+		return isSubPathFunc2(primitive, primitive, root.Left) ||
+			isSubPathFunc2(primitive, primitive, root.Right)
+	} else {
+		return isSubPathFunc2(primitive, primitive, root) ||
+			isSubPathFunc2(primitive, primitive, root)
+	}
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode

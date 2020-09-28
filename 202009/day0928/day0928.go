0df677947e6bda5f898401c2799c1e0eedbf3109
tanhao <tanhao@wondersgroup.com>
2020-09-28 10:57:39 +0800

    59.螺旋矩阵2-用rust语言解答

--- 202009/day0928/day0928.go
+++ 202009/day0928/day0928.go
@@ -11,6 +11,66 @@ type Node struct {
 	Next  *Node
 }
 
+/**螺旋矩阵2,rust解决
+pub fn generate_matrix(n: i32) -> Vec<Vec<i32>> {
+        //let mut res: Vec<Vec<i32>> = Vec::new();
+        ////初始化
+        //for i in 0..n{
+        //    let mut item = Vec::new();
+        //    for i in 0 .. n{
+        //        item.insert(i as usize,0);
+        //    }
+        //    res.insert(i as usize,item);
+        //}
+		//定义二维向量
+		let mut res= vec![vec![0;n as usize];n as usize];
+
+        let mut count = 1;
+        let mut x = 0;
+        let mut y = 0;
+
+        let mut nn = n;
+        while count <= n * n{
+
+            //先 向右走n步
+            for i in 0..nn {
+
+                res[x][y] = count;
+                count += 1;
+                y += 1;
+            }
+
+            y -= 1;
+
+            //向下走n -1步
+            for i in 0..nn-1{
+                x += 1;
+                res[x][y] = count;
+                count +=1;
+            }
+
+            //向左走n-1步
+            for i in 0..nn-1{
+                y -=1;
+                res[x][y]= count;
+                count +=1;
+            }
+
+            //向上走n-2步
+            for i in 0 .. nn -2{
+                x-=1;
+                res[x][y] = count;
+                count +=1;
+            }
+
+            nn -= 2;
+            y += 1;
+        }
+
+        return res;
+    }
+ */
+
 func connect(root *Node) *Node {
 	if root == nil {
 		return root

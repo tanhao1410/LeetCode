176887dc67e3c7155e632194a1a6e0ebded0b3f3
tanhao1410 <2528865013@qq.com>
2020-09-30 07:15:04 +0800

    38.外观数列

--- 202009/day0930/day0930.go
+++ 202009/day0930/day0930.go
@@ -1,7 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+func main() {
+	fmt.Println(countAndSay(5))
+}
+
+//外观数列
+func countAndSay(n int) string {
+	m := [][]int8{{1},{1,1}}
+	for i := 2;i < n ;i ++{
+		item,pre := []int8{}, m [i -1]
+		//需要看它的前一项的情况
+		var count int8 = 1
+		var num int8 = pre[0]
+		for j := 1;j < len(pre) - 1;j ++{
+			if pre[j] == num {
+				count ++
+			}else{
+				item = append(item, count,num)
+				num,count = pre[j],1
+			}
+		}
+		item = append(item, count,num)
+		m = append(m, item)
+	}
+
+	res := ""
+	for i := 0 ;i < len(m[n -1]);i ++{
+		res += strconv.Itoa(int(m[n -1][i]))
+	}
+	return res
 }
 
 type TreeNode struct {

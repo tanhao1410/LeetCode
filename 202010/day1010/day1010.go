49662331bd13b345e0a05f5911142b40f323d260
tanhao <tanhao@wondersgroup.com>
2020-10-10 09:20:50 +0800

    49.字母异位词分组

--- 202010/day1010/day1010.go
+++ 202010/day1010/day1010.go
@@ -1,7 +1,47 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	strs := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+
+	fmt.Println(groupAnagrams(strs))
+}
+
+//字母异味词分组
+func groupAnagrams(strs []string) [][]string {
+	//打散，排序，hash
+	res := [][]string{}
+	//key:打散排序后的字符串，value：所有异分词集合
+	m  := make(map[string][]string)
+	for i:= 0 ;i < len(strs);i++{
+		sortedString := CreateSortedString(strs[i])
+		if item,ok:= m[sortedString];ok{
+			item = append(item, strs[i])
+			m[sortedString] = item
+		}else{
+			item := []string{strs[i]}
+			m[sortedString] = item
+		}
+	}
+
+	for _,v := range m{
+		res = append(res, v)
+	}
+
+	return res
+}
+
+func CreateSortedString(s string) string{
+	bytes := []byte(s);
+	for i:=0;i < len(bytes) - 1;i ++{
+		for j:=i;j< len(bytes);j++{
+			if bytes[j] < bytes[i]{
+				bytes[j],bytes[i] = bytes[i],bytes[j]
+			}
+		}
+	}
+	return string(bytes)
 }
 
 type ListNode struct {

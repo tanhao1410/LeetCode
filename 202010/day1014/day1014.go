58b852d7c65a22c43dfb0c5d3840addf0d66c6ea
tanhao <tanhao@wondersgroup.com>
2020-10-14 09:51:33 +0800

    80.删除排序数组中的重复项 II

--- 202010/day1014/day1014.go
+++ 202010/day1014/day1014.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println(commonChars([]string{"cool","lock","cook"}))
+	//fmt.Println(commonChars([]string{"cool", "lock", "cook"}))
+	fmt.Println(removeDuplicates([]int{1,1,1,2,2,3}))
+}
+
+//80.删除排序数组中的重复项 II
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	//最多出现2次
+	//记录前面的出现的次数，当出现2次了，和当前又相同，说明，该数需要删除
+	//需要双指针了
+	flag := math.MaxInt32
+	j,i := 1,0
+	for ; j < len(nums); {
+		if nums[j] != nums[i]{
+			flag = nums[i]
+			nums[i+1] = nums[j]
+			j++
+			i++
+		}else if nums[i] == nums[j] && flag != nums[j]{
+			flag = nums[i]
+			nums[i+1] = nums[j]
+			j++
+			i++
+		}else{
+			j++
+		}
+	}
+	return i+1
 }
 
 //1002.查找常用字符

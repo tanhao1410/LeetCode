e33b27aabf0b495d2e032df92d9cd8f91bd3f0b3
tanhao <tanhao@wondersgroup.com>
2020-10-16 10:44:49 +0800

    90.子集 II-暂未解决。。。

--- 202010/day1016/day1016.go
+++ 202010/day1016/day1016.go
@@ -1,7 +1,58 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums := []int{1,2,2,3}
+	fmt.Println(subsetsWithDup(nums))
+}
+
+//90.子集 II
+func subsetsWithDup(nums []int) [][]int {
+	//子集可能包含重复元素
+	//先排序
+	//只能选择比自己大的，所有的都按照有序来选择
+	res := [][]int{[]int{}}
+	sortNums(nums)
+	nextNum([]int{},nums,&res)
+	return res
+}
+
+func nextNum(have,remain []int,res *[][]int){
+	if len(remain) == 0{
+		return
+	}
+	for i:=0;i < len(remain);i ++{
+		if i != 0 && remain[i] == remain[i-1]{
+			continue
+		}
+		if len(have) != 0 && remain[i] < have[len(have)-1]{
+			continue
+		}
+		have2 := make([]int,len(have))
+		copy(have2,have)
+		have2 = append(have2, remain[i])
+		*res = append(*res, have2)
+		var remain2 []int;
+		if i == 0{
+			remain2 = remain[1:]
+		}else if i == len(remain)-1{
+			remain2 = remain[:i]
+		}else{
+			remain2= append(remain[:i],remain[i+1:]...)
+		}
+		nextNum(have2,remain2,res)
+	}
+}
+
+func sortNums(nums []int){
+	for i:=0;i < len(nums) -1;i ++{
+		for j := i+1;j <len(nums);j ++{
+			if nums[i] > nums[j]{
+				nums[i],nums[j] = nums[j],nums[i]
+			}
+		}
+	}
 }
 
 //977.有序数组的平方

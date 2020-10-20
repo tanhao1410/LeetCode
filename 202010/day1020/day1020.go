230d08e33cb61e3c6f88afb5a82e99d7ca8da73e
tanhao <tanhao@wondersgroup.com>
2020-10-20 13:06:05 +0800

    56.合并区间

--- 202010/day1020/day1020.go
+++ 202010/day1020/day1020.go
@@ -4,16 +4,64 @@ func main() {
 
 }
 
+//56.合并区间
+func merge(intervals [][]int) [][]int {
+
+	flag := true
+A:
+	for flag {
+		for i := 0; i < len(intervals)-1; i++ {
+			interval := intervals[i]
+			min, max := interval[0], interval[1]
+			//和它后面的进行比较
+			for j := i + 1; j < len(intervals); j++ {
+				interval2 := intervals[j]
+				min2, max2 := interval2[j], interval2[1]
+				if min2 > max || min > max2 {
+					//没重合
+				} else {
+					//重合了
+					newInterval := []int{}
+					if min < min2 {
+						newInterval = append(newInterval, min)
+					} else {
+						newInterval = append(newInterval, min2)
+					}
+					if max > max2 {
+						newInterval = append(newInterval, max)
+					} else {
+						newInterval = append(newInterval, max2)
+					}
+
+					newIntervals := make([][]int, len(intervals)-1)
+					for k, l := 0, 0; k < len(intervals); k++ {
+						if k != i && k != j {
+							newIntervals[l] = intervals[k]
+							l++
+						}
+					}
+					newIntervals[len(intervals)-2] = newInterval
+					intervals = newIntervals
+					continue A
+				}
+			}
+		}
+		flag = false
+	}
+
+	return intervals
+}
+
 //100.相同的树
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if p != nil && q != nil{
-		if p.Val != q.Val{
+	if p != nil && q != nil {
+		if p.Val != q.Val {
 			return false
 		}
-		return isSameTree(p.Right,q.Right)&&isSameTree(p.Left,q.Left)
-	}else if p == nil && q == nil{
+		return isSameTree(p.Right, q.Right) && isSameTree(p.Left, q.Left)
+	} else if p == nil && q == nil {
 		return true
-	}else{
+	} else {
 		return false
 	}
 }

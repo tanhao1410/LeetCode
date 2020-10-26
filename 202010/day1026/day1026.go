5858cfe5413795a5ce067aa2551d9d8eca3d5db0
tanhao <tanhao@wondersgroup.com>
2020-10-26 09:13:15 +0800

    48.旋转图像

--- 202010/day1026/day1026.go
+++ 202010/day1026/day1026.go
@@ -1,8 +1,44 @@
 package main
 
 func main() {
-	nums := []int{5, 0, 10, 0, 10, 6}
-	print(smallerNumbersThanCurrent(nums))
+	//nums := []int{5, 0, 10, 0, 10, 6}
+	//print(smallerNumbersThanCurrent(nums))
+	matrix := [][]int{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}}
+	rotate(matrix)
+}
+
+//48.旋转图像
+func rotate(matrix [][]int) {
+	//先旋转外层，再递归旋转里层，直到里层为空或1
+	l := len(matrix)
+	if l < 2 {
+		return
+	}
+
+	//列比行多
+	if l < len(matrix[0]) {
+
+		//行和原来一样，取列的时候，都加上一个数，
+		n := (len(matrix[0]) - l) / 2
+		for i := 0; i < l-1; i++ {
+			matrix[0][i+n], matrix[l-1-i][n], matrix[l-1][l-1-i+n], matrix[i][l-1+n] =
+				matrix[l-1-i][n], matrix[l-1][l-1-i+n], matrix[i][l-1+n], matrix[0][i+n]
+		}
+	} else {
+		//旋转外层
+		for i := 0; i < l-1; i++ {
+			matrix[0][i], matrix[l-1-i][0], matrix[l-1][l-1-i], matrix[i][l-1] =
+				matrix[l-1-i][0], matrix[l-1][l-1-i], matrix[i][l-1], matrix[0][i]
+		}
+	}
+
+	//旋转内层
+	if l > 3 {
+		//抽取行
+		miniMatrix := matrix[1 : l-1]
+		rotate(miniMatrix)
+	}
+
 }
 
 //1365.有多少小于当前数字的数字

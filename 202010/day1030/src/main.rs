d48084c962ec2cc71dc0fa18c557b5ff64ef6c83
tanhao <tanhao@wondersgroup.com>
2020-10-30 08:58:49 +0800

    剑指 Offer 12. 矩阵中的路径

--- 202010/day1030/src/main.rs
+++ 202010/day1030/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}",Solution::cutting_rope2(1000));
 }
 
 struct Solution{
@@ -26,7 +27,59 @@ impl Solution {
         dp[n as usize]
     }
 
-    //每日一题；463 岛屿的周长
+    //剑指 Offer 12. 矩阵中的路径
+    pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
+        let mut board = board;
+        //回溯法，先找到第一个可以走的，再走，遇到不满足的就回退
+        fn next(board: &mut Vec<Vec<char>>, m: usize, n: usize, word: &str) -> bool {
+            if word.len() == 0 {
+                return true;
+            }
+            //找下一个可以走的地方符合的
+            let mut can = vec![];
+            //往下
+            if m + 1 < board.len() && board[m + 1][n] == word.chars().nth(0).unwrap() {
+                can.push((m + 1, n));
+            }
+            if m as i32 - 1 >= 0 && board[m - 1][n] == word.chars().nth(0).unwrap() {
+                can.push((m - 1, n));
+            }
+            if n + 1 < board[0].len() && board[m][n + 1] == word.chars().nth(0).unwrap() {
+                can.push((m , n+1));
+            }
+            if n as i32 - 1 >= 0 && board[m][n - 1] == word.chars().nth(0).unwrap() {
+                can.push((m , n-1));
+            }
+            if can.len()==0{
+                return false;
+            }
+
+            for i in can.into_iter(){
+                board[i.0][i.1] = ' ';
+                if next(board,i.0,i.1,&word[1..]){
+                    return true;
+                }
+                board[i.0][i.1] = word.chars().nth(0).unwrap();
+            }
+            false
+        }
+
+        let (m, n) = (board.len(), board[0].len());
+        for i in 0..m {
+            for j in 0..n {
+                if board[i][j] == word.chars().nth(0).unwrap() {
+                    board[i][j] = ' ';
+                    if next(&mut board, i, j, &word[1..]) {
+                        return true;
+                    }
+                    board[i][j] = word.chars().nth(0).unwrap();
+                }
+            }
+        }
+        false
+    }
+
+    //每日一题；463 岛屿的周长:新思路：左右和上下其实是相等的，减少一遍求。2.也可以是采用总的岛屿数*4 - 重合的边*2
     pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
         if grid.len() == 0 || grid[0].len() == 0{
             return 0

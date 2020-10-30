daf5e750c4ad3ad5bc0664b36d0e665f3dc0541a
tanhao <tanhao@wondersgroup.com>
2020-10-30 15:40:30 +0800

    剑指 Offer 47. 礼物的最大价值

--- 202010/day1030/src/main.rs
+++ 202010/day1030/src/main.rs
@@ -1,13 +1,36 @@
 fn main() {
     println!("Hello, world!");
-    println!("{}",Solution::cutting_rope2(1000));
+    //println!("{}", Solution::translate_num(12323));
 }
 
-struct Solution{
+struct Solution {}
 
-}
 
 impl Solution {
+    //剑指 Offer 47. 礼物的最大价值
+    pub fn max_value(grid: Vec<Vec<i32>>) -> i32 {
+        //思路：普通动态规划题目
+        let mut dp = grid.clone();
+        let (m, n) = (grid.len() - 1, grid[0].len() - 1);
+        for i in (0..m + 1).rev() {
+            for j in (0..n + 1).rev() {
+                if j == n && i == m {} else if i == m && j < n {
+                    //最后一行
+                    dp[m][j] = dp[m][j + 1] + grid[i][j];
+                } else if i < m && j == n {
+                    //最后一列
+                    dp[i][j] = dp[i + 1][j] + grid[i][j];
+                } else {
+                    if dp[i + 1][j] > dp[i][j + 1] {
+                        dp[i][j] = dp[i + 1][j] + grid[i][j];
+                    } else {
+                        dp[i][j] = dp[i][j + 1] + grid[i][j];
+                    }
+                }
+            }
+        }
+        dp[0][0]
+    }
 
     //剑指 Offer 14- I. 剪绳子
     pub fn cutting_rope(n: i32) -> i32 {
@@ -45,18 +68,18 @@ impl Solution {
                 can.push((m - 1, n));
             }
             if n + 1 < board[0].len() && board[m][n + 1] == word.chars().nth(0).unwrap() {
-                can.push((m , n+1));
+                can.push((m, n + 1));
             }
             if n as i32 - 1 >= 0 && board[m][n - 1] == word.chars().nth(0).unwrap() {
-                can.push((m , n-1));
+                can.push((m, n - 1));
             }
-            if can.len()==0{
+            if can.len() == 0 {
                 return false;
             }
 
-            for i in can.into_iter(){
+            for i in can.into_iter() {
                 board[i.0][i.1] = ' ';
-                if next(board,i.0,i.1,&word[1..]){
+                if next(board, i.0, i.1, &word[1..]) {
                     return true;
                 }
                 board[i.0][i.1] = word.chars().nth(0).unwrap();
@@ -81,53 +104,53 @@ impl Solution {
 
     //每日一题；463 岛屿的周长:新思路：左右和上下其实是相等的，减少一遍求。2.也可以是采用总的岛屿数*4 - 重合的边*2
     pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
-        if grid.len() == 0 || grid[0].len() == 0{
-            return 0
+        if grid.len() == 0 || grid[0].len() == 0 {
+            return 0;
         }
         //思路：从四个方向分别求出对应的边长，然后相加
-        let  (mut left_num,mut right_num,mut up_num,mut down_num) = (0,0,0,0);
-        for i in 0..grid.len(){
-            let (mut left_flag,mut right_flag) = (true,true);
-            for j in 0..grid[0].len(){
-                if grid[i][j] == 1 && left_flag{
-                    left_num +=1;
+        let (mut left_num, mut right_num, mut up_num, mut down_num) = (0, 0, 0, 0);
+        for i in 0..grid.len() {
+            let (mut left_flag, mut right_flag) = (true, true);
+            for j in 0..grid[0].len() {
+                if grid[i][j] == 1 && left_flag {
+                    left_num += 1;
                     left_flag = false;
                 }
-                if grid[i][j] == 0{
+                if grid[i][j] == 0 {
                     left_flag = true;
                 }
             }
-            for j in (0..grid[0].len()).rev(){
-                if grid[i][j] == 1 && right_flag{
-                    right_num +=1;
+            for j in (0..grid[0].len()).rev() {
+                if grid[i][j] == 1 && right_flag {
+                    right_num += 1;
                     right_flag = false;
                 }
-                if grid[i][j] == 0{
+                if grid[i][j] == 0 {
                     right_flag = true;
                 }
             }
         }
-        for i in 0..grid[0].len(){
-            let (mut up_flag,mut down_flag) = (true,true);
-            for j in 0..grid.len(){
-                if grid[j][i] == 1 && up_flag{
-                    up_num +=1;
+        for i in 0..grid[0].len() {
+            let (mut up_flag, mut down_flag) = (true, true);
+            for j in 0..grid.len() {
+                if grid[j][i] == 1 && up_flag {
+                    up_num += 1;
                     up_flag = false;
                 }
-                if grid[j][i] == 0{
+                if grid[j][i] == 0 {
                     up_flag = true;
                 }
             }
-            for j in 0..grid.len(){
-                if grid[j][i] == 1 && down_flag{
-                    down_num +=1;
+            for j in 0..grid.len() {
+                if grid[j][i] == 1 && down_flag {
+                    down_num += 1;
                     down_flag = false;
                 }
-                if grid[j][i] == 0{
+                if grid[j][i] == 0 {
                     down_flag = true;
                 }
             }
         }
-        left_num+right_num+up_num+down_num
+        left_num + right_num + up_num + down_num
     }
 }

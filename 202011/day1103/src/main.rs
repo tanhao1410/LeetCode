dd3d2ab55573e2116ac06371fce98ac281886817
tanhao1410 <2528865013@qq.com>
2020-11-03 07:09:51 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 II

--- 202011/day1103/src/main.rs
+++ 202011/day1103/src/main.rs
@@ -24,4 +24,52 @@ impl Solution {
         }
         !is_increase
     }
+
+    //剑指 Offer 32 - II. 从上到下打印二叉树 II
+    pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        let mut res = Vec::new();
+        if root.is_none() {
+            return res;
+        }
+        let mut queue = vec![root];
+        let mut queue_len = queue.len();
+        while queue_len > 0 {
+            let mut res_item = vec![];
+            for i in 0..queue_len {
+                let v = queue.remove(0).unwrap();
+                res_item.push(v.borrow_mut().val);
+                if v.borrow_mut().left.is_some() {
+                    queue.push(v.borrow_mut().left.clone());
+                }
+                if v.borrow_mut().right.is_some() {
+                    queue.push(v.borrow_mut().right.clone());
+                }
+            }
+            res.push(res_item);
+            queue_len = queue.len();
+        }
+        res
+    }
 }
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::borrow::Borrow;

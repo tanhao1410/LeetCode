67ff036677e4de56c5dcca62847dde48bea226e0
tanhao <tanhao@wondersgroup.com>
2020-11-05 14:58:01 +0800

    127. 单词接龙-优化版本

--- 202011/day1105/java/Solution.java
+++ 202011/day1105/java/Solution.java
@@ -16,6 +16,49 @@ public class Solution {
         System.out.println(new Solution().ladderLength("hit","cog",wordList));
     }
 
+    //优化版本
+    public int ladderLength2(String beginWord, String endWord, List<String> wordList) {
+            HashSet<String> wordSet = new HashSet<String>(wordList);
+            if (!wordSet.contains(endWord)) {
+                return 0;
+            }
+            HashMap<Integer, HashSet<String>> dp = new HashMap<>();
+            HashSet<String> one = new HashSet();
+            one.add(endWord);
+            wordSet.remove(endWord);
+            wordSet.add(beginWord);
+            dp.put(1, one);
+            int count = 1;
+            while (dp.get(count) != null) {
+                HashSet<String> pre = dp.get(count);
+                for (String s : pre) {
+                    for (int j = 0; j < s.length(); j++) {
+                        char[] chars = s.toCharArray();
+                        for (char c = 'a'; c <= 'z'; c++) {
+                            chars[j] = c;
+                            String canReach = new String(chars);
+                            if (wordSet.contains(canReach)) {
+                                //可以到达
+                                wordSet.remove(canReach);
+                                if (canReach.equals(beginWord)) {
+                                    return count + 1;
+                                }
+                                if (dp.get(count + 1) == null) {
+                                    HashSet<String> nextStrings = new HashSet<>();
+                                    nextStrings.add(canReach);
+                                    dp.put(count + 1, nextStrings);
+                                } else {
+                                    dp.get(count + 1).add(canReach);
+                                }
+                            }
+                        }
+                    }
+                }
+                count += 1;
+            }
+            return 0;
+        }
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         HashSet<String> wordSet = new HashSet<String>(wordList);
         if (!wordSet.contains(endWord)){

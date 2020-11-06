c5df7d0abb07603305c4f848f8bcb350b922597e
tanhao <tanhao@wondersgroup.com>
2020-11-06 12:56:11 +0800

    剑指 Offer 15. 二进制中1的个数

--- 202011/day1106/src/main.rs
+++ 202011/day1106/src/main.rs
@@ -6,6 +6,19 @@ fn main() {
 struct Solution {}
 
 impl Solution {
+
+    //剑指 Offer 15. 二进制中1的个数 191. 位1的个数 9 1001 2
+    //n&(n−1) 解析： 二进制数字 nn 最右边的 11 变成 00 ，其余不变。
+    pub fn hammingWeight(mut n: u32) -> i32 {
+        //思路：&1，然后看等于即
+        let mut res = 0;
+        while n != 0 {
+            res += n & 1;
+            n>>=1;
+        }
+        res as i32
+    }
+
     //1356. 根据数字二进制下 1 的数目排序
     pub fn sort_by_bits(mut arr: Vec<i32>) -> Vec<i32> {
         fn count_binary1(mut num :i32)->i32{

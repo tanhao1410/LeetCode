a340d0c845bb0b0d291a722b707074252494240d
tanhao <tanhao@wondersgroup.com>
2020-11-19 08:43:04 +0800

    227. 基本计算器 II

--- 202011/day1119/day1119.go
+++ 202011/day1119/day1119.go
@@ -1,7 +1,65 @@
 package main
 
-func main() {
+import (
+	"strconv"
+	"strings"
+)
 
+func main() {
+	//fmt.Println(calculate("3 *1 /2"))
+}
+
+//227. 基本计算器 II
+func calculate(s string) int {
+	//仅包含+-*/
+	//思路：注意顺序即可
+	//去除所有空格，
+	s1 := strings.ReplaceAll(s, " ", "")
+	ss := []string{}
+	start := 0
+	for i := 0; i < len(s1); i++ {
+		if s1[i] == '+' || s1[i] == '-' || s1[i] == '*' || s1[i] == '/' {
+			//需要截取数字了
+			ss = append(ss, s1[start:i])
+			ss = append(ss, s1[i:i+1])
+			start = i + 1
+		}
+	}
+	ss = append(ss, s1[start:])
+	stack := []string{ss[0]}
+	for i := 1; i < len(ss); i++ {
+		cur := ss[i]
+		//看栈顶的情况
+		top := stack[len(stack)-1]
+		if top == "*" {
+			//取出来栈的两个元素，第一个为*号，第二为应有的数字
+			num, _ := strconv.Atoi(stack[len(stack)-2])
+			curNum, _ := strconv.Atoi(cur)
+			stack = stack[:len(stack)-1]
+			stack[len(stack)-1] = strconv.Itoa(num * curNum)
+			//结果入栈
+		} else if top == "/" {
+			num, _ := strconv.Atoi(stack[len(stack)-2])
+			curNum, _ := strconv.Atoi(cur)
+			stack = stack[:len(stack)-1]
+			stack[len(stack)-1] = strconv.Itoa(num / curNum)
+		} else {
+			stack = append(stack, cur)
+		}
+	}
+
+	res, _ := strconv.Atoi(stack[0])
+	//循环结束后，栈中只剩下+-了
+	for i := 1; i < len(stack); i++ {
+		if stack[i] == "+" {
+			nextNum, _ := strconv.Atoi(stack[i+1])
+			res += nextNum
+		} else if stack[i] == "-" {
+			nextNum, _ := strconv.Atoi(stack[i+1])
+			res -= nextNum
+		}
+	}
+	return res
 }
 
 //每日一题：283. 移动零

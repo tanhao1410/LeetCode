912aa5f7ad331a814fc3b56ca553244a840bdeea
tanhao <tanhao@wondersgroup.com>
2020-11-24 13:13:19 +0800

    149. 直线上最多的点数

--- 202011/day1124/day1124.go
+++ 202011/day1124/day1124.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 0, 1, 2, 0, 0}
-	sortColors(nums)
-	fmt.Println(nums)
+	//nums := []int{1, 0, 1, 2, 0, 0}
+	//sortColors(nums)
+	//fmt.Println(nums)
+	fmt.Println(maxPoints([][]int{{-6, -65}, {-18, 26}, {-6, -65}, {10, -2}, {10, -2}}))
 }
 
 type TreeNode struct {
@@ -46,4 +47,49 @@ func sortColors(nums []int) {
 			nums[i] = 2
 		}
 	}
+}
+
+//149. 直线上最多的点数
+func maxPoints(points [][]int) int {
+	//思路：
+	//两个点相同的时候，会出现，第三个点一定在同一直线上的问题。
+	isOneLine := func(p1, p2, p3 []int) bool {
+		return (p3[1]-p1[1])*(p2[0]-p1[0]) ==
+			(p2[1]-p1[1])*(p3[0]-p1[0])
+	}
+	if len(points) < 3 {
+		return len(points)
+	}
+	res := 0
+	//用一个数组记录哪个线段被用了
+	//先从第一个点开始
+	for k, first := range points {
+
+		//找下一个点，以凑成线
+		samePointNum := 0
+		for i := k + 1; i < len(points); i++ {
+			//即这个点已经和刚才形成了线了，不用重复计算了
+			second := points[i]
+			//如果第一条线和第二条线是重合的话
+			if second[0] == first[0] && second[1] == first[1] {
+				samePointNum += 1
+				continue
+			}
+			//所有的点都是同一个点的话，直接就结束了
+			count := 2 + samePointNum
+			//samePointNum = 0
+			for third := i + 1; third < len(points); third++ {
+				if isOneLine(first, second, points[third]) {
+					count++
+				}
+			}
+			if count > res {
+				res = count
+			}
+		}
+		if samePointNum+1 > res {
+			res = samePointNum + 1
+		}
+	}
+	return res
 }

692ddda4618b93283b96c561bbb8e8018a004310
tanhao <tanhao@wondersgroup.com>
2020-11-25 10:33:31 +0800

    289. 生命游戏

--- 202011/day1125/day1125.go
+++ 202011/day1125/day1125.go
@@ -3,7 +3,69 @@ package main
 import "fmt"
 
 func main() {
+	board := [][]int{{1, 1, 0}, {1, 0, 1}, {1, 1, 1}, {0, 0, 0}}
+	gameOfLife(board)
+}
 
+//289. 生命游戏
+func gameOfLife(board [][]int) {
+	if len(board) == 0 || len(board[0]) == 0 {
+		return
+	}
+
+	m, n := len(board), len(board[0])
+	//思路：可以用置2来解决本地的问题,2代表原来是死的现在变活了，3代表原来是活的，现在死了
+	liveCount := func(x, y int) int {
+		count := 0
+
+		if x-1 >= 0 && (board[x-1][y] == 1 || board[x-1][y] == 3) {
+			count += 1
+		}
+		if x-1 >= 0 && y-1 >= 0 && (board[x-1][y-1] == 1 || board[x-1][y-1] == 3) {
+			count++
+		}
+		if x-1 >= 0 && y+1 < n && (board[x-1][y+1] == 1 || board[x-1][y+1] == 3) {
+			count++
+		}
+		if y-1 >= 0 && (board[x][y-1] == 1 || board[x][y-1] == 3) {
+			count++
+		}
+		if y+1 < n && (board[x][y+1] == 1 || board[x][y+1] == 3) {
+			count++
+		}
+		if x+1 < m && (board[x+1][y] == 1 || board[x+1][y] == 3) {
+			count++
+		}
+		if x+1 < m && y-1 >= 0 && (board[x+1][y-1] == 1 || board[x+1][y-1] == 3) {
+			count++
+		}
+		if x+1 < m && y+1 < n && (board[x+1][y+1] == 1 || board[x+1][y+1] == 3) {
+			count++
+		}
+		return count
+	}
+
+	for x, row := range board {
+		for y, v := range row {
+			count := liveCount(x, y)
+			//如果活细胞周围八个位置的活细胞数少于两个，则该位置活细胞死亡；
+			//活细胞周围八个位置有超过三个活细胞
+			if (count < 2 || count > 3) && v == 1 {
+				board[x][y] = 3
+			} else if v == 0 && count == 3 {
+				board[x][y] = 2
+			}
+		}
+	}
+	for x, row := range board {
+		for y, v := range row {
+			if v == 2 {
+				board[x][y] = 1
+			} else if v == 3 {
+				board[x][y] = 0
+			}
+		}
+	}
 }
 
 //299. 猜数字游戏

580c5f2ae1f4a0bd1a2f943d491cbf4f5299aff1
tanhao <tanhao@wondersgroup.com>
2020-11-27 10:02:03 +0800

    剑指 Offer 09. 用两个栈实现队列

--- 202011/day1127/day1127.go
+++ 202011/day1127/day1127.go
@@ -4,6 +4,39 @@ func main() {
 
 }
 
+//剑指 Offer 09. 用两个栈实现队列
+type CQueue struct {
+	Stack1 []int
+	Stack2 []int
+	//思路，两个栈，进队时，直接进push进1，出队时，先将栈1全部pop到栈2，然后弹出栈2的顶部数据，剩余的再弹回栈1
+}
+
+func Constructor() CQueue {
+	return CQueue{
+		[]int{}, []int{},
+	}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.Stack1 = append(this.Stack1, value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.Stack1) == 0 && len(this.Stack2) == 0 {
+		return -1
+	}
+	if len(this.Stack2) == 0 {
+		for len(this.Stack1) > 0 {
+			popNum := this.Stack1[len(this.Stack1)-1]
+			this.Stack1 = this.Stack1[:len(this.Stack1)-1]
+			this.Stack2 = append(this.Stack2, popNum)
+		}
+	}
+	res := this.Stack2[len(this.Stack2)-1]
+	this.Stack2 = this.Stack2[:len(this.Stack2)-1]
+	return res
+}
+
 //275. H 指数 II
 func hIndex(citations []int) int {
 	//已经是排好序的了

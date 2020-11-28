160e410e966bd6c80c93107965c23beae9c4df67
tanhao1410 <2528865013@qq.com>
2020-11-28 14:51:58 +0800

    304. 二维区域和检索 - 矩阵不可变

--- 202011/day1128/src/main.rs
+++ 202011/day1128/src/main.rs
@@ -3,13 +3,11 @@ fn main() {
     unsafe {
         println!("{}", Solution::guessNumber(2126753390))
     }
-
 }
 
 struct Solution {}
 
 impl Solution {
-
     //374. 猜数字大小
     unsafe fn guessNumber(n: i32) -> i32 {
         unsafe fn guessNum(min: i32, max: i32) -> i32 {
@@ -18,9 +16,9 @@ impl Solution {
             if guess(middle) == 0 {
                 return middle;
             } else if guess(middle) < 0 {
-                return guessNum(min,middle - 1);
+                return guessNum(min, middle - 1);
             }
-            return guessNum(middle+1,max);
+            return guessNum(middle + 1, max);
         }
         guessNum(1, n)
     }
@@ -60,5 +58,41 @@ impl Solution {
 }
 
 unsafe fn guess(num: i32) -> i32 {
-    return 1702766719-num
+    return 1702766719 - num;
+}
+
+//304. 二维区域和检索 - 矩阵不可变
+struct NumMatrix {
+    matrix: Vec<Vec<i32>>
+}
+
+impl NumMatrix {
+    fn new(matrix: Vec<Vec<i32>>) -> Self {
+        NumMatrix {
+            matrix: matrix
+        }
+    }
+
+    fn sum_region(&self, mut row1: i32, mut col1: i32, row2: i32, col2: i32) -> i32 {
+        //没有元素时，直接返回0
+        if self.matrix.len() == 0 || self.matrix[0].len() == 0 || row2 < 0 || col2 < 0
+            || row2 >= self.matrix.len() as i32 || col2 >= self.matrix[0].len() as i32 {
+            return 0;
+        }
+        let mut res = 0;
+
+        if row1 < 0 {
+            row1 == 0
+        }
+        if col1 < 0 {
+            col1 == 0
+        }
+
+        for i in row1..row2 + 1 {
+            for j in col1..col2 + 1 {
+                res += self.matrix[i as usize][j as usize]
+            }
+        }
+        res
+    }
 }

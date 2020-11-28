65d802b96ccf2e9b8802837f596227f61758f7de
tanhao1410 <2528865013@qq.com>
2020-11-28 11:33:02 +0800

    374. 猜数字大小

--- 202011/day1128/src/main.rs
+++ 202011/day1128/src/main.rs
@@ -1,17 +1,37 @@
 fn main() {
     println!("Hello, world!");
+    unsafe {
+        println!("{}", Solution::guessNumber(2126753390))
+    }
+
 }
 
 struct Solution {}
 
 impl Solution {
+
+    //374. 猜数字大小
+    unsafe fn guessNumber(n: i32) -> i32 {
+        unsafe fn guessNum(min: i32, max: i32) -> i32 {
+            //要考虑越界的问题；
+            let middle = (max - min) / 2 + min;
+            if guess(middle) == 0 {
+                return middle;
+            } else if guess(middle) < 0 {
+                return guessNum(min,middle - 1);
+            }
+            return guessNum(middle+1,max);
+        }
+        guessNum(1, n)
+    }
+
     //每日一题：493. 翻转对
     pub fn reverse_pairs(nums: Vec<i32>) -> i32 {
         //暴力思想。时间超时无法完成。
         let mut count = 0;
         for i in 0..nums.len() - 1 {
             for j in i + 1..nums.len() {
-                if nums[i] >> 1 > nums[j] || (nums[i]  >>1 == nums[j] && nums[i] % 2 == 1) {
+                if nums[i] >> 1 > nums[j] || (nums[i] >> 1 == nums[j] && nums[i] % 2 == 1) {
                     count += 1
                 }
             }
@@ -37,4 +57,8 @@ impl Solution {
 
         ('a' as u8 + index as u8) as char
     }
+}
+
+unsafe fn guess(num: i32) -> i32 {
+    return 1702766719-num
 }

45872c6599ec352dd915301d52948715e9afe54f
tanhao1410 <2528865013@qq.com>
2020-11-29 09:19:00 +0800

    剑指 Offer 30. 包含min函数的栈

--- 202011/day1129/src/main.rs
+++ 202011/day1129/src/main.rs
@@ -4,34 +4,82 @@ fn main() {
 
 struct Solution {}
 
-//155. 最小栈
-struct MinStack {
-    stack :Vec<i32>,
-    min:i32
+//剑指 Offer 30. 包含min函数的栈
+struct MinStack2 {
+    data: Vec<i32>,
+    min: Vec<i32>,
 }
-impl MinStack {
 
+impl MinStack2 {
     /** initialize your data structure here. */
     fn new() -> Self {
-        MinStack{
-            stack:vec![],
-            min:std::i32::MAX
+        MinStack2 {
+            data: vec![],
+            min: vec![],
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.data.push(x);
+        if self.min.is_empty() {
+            self.min.push(x);
+        } else {
+            let top = *self.min.last().unwrap();
+            if x < top {
+                self.min.push(x);
+            } else {
+                self.min.push(top);
+            }
+        }
+    }
+
+    fn pop(&mut self) {
+        if !self.data.is_empty() {
+            self.data.pop();
+        }
+        if !self.min.is_empty() {
+            self.min.pop();
+        }
+    }
+
+    fn top(&self) -> i32 {
+        *self.data.last().unwrap()
+    }
+
+    fn min(&self) -> i32 {
+        *self.min.last().unwrap()
+    }
+}
+
+
+//155. 最小栈
+struct MinStack {
+    stack: Vec<i32>,
+    min: i32,
+}
+
+impl MinStack {
+    /** initialize your data structure here. */
+    fn new() -> Self {
+        MinStack {
+            stack: vec![],
+            min: std::i32::MAX,
         }
     }
 
     fn push(&mut self, x: i32) {
         self.stack.push(x);
-        if x < self.min{
+        if x < self.min {
             self.min = x;
         }
     }
 
     fn pop(&mut self) {
         let popValue = self.stack.pop().unwrap();
-        if popValue == self.min{
+        if popValue == self.min {
             self.min = std::i32::MAX;
-            for i in &self.stack{
-                if *i < self.min{
+            for i in &self.stack {
+                if *i < self.min {
                     self.min = *i;
                 }
             }

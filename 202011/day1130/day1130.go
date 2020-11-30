e1bbfb8f5a17853b103f8873a1718e94f8c8c91c
tanhao <tanhao@wondersgroup.com>
2020-11-30 10:35:34 +0800

    201. 数字范围按位与

--- 202011/day1130/day1130.go
+++ 202011/day1130/day1130.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println(reorganizeString("ababa"))
+	fmt.Println(rangeBitwiseAnd(0, 1))
+}
+
+//201. 数字范围按位与
+func rangeBitwiseAnd(m int, n int) int {
+	res := 0
+	//高位
+	//m,n,从高位往低比较，直到不相等的，前面相等的作为结果的高位
+	//也就是说后面全都是0？
+	//first := math.MinInt32
+	var first uint32 = 0b1000_0000_0000_0000_0000_0000_0000_0000
+	for i := 31; i >= 0 && uint32(m)&first == uint32(n)&first; i-- {
+		//求第一位
+		res += int(uint32(m) & first)
+		first >>= 1
+	}
+	return res
 }
 
 //每日一题：767. 重构字符串

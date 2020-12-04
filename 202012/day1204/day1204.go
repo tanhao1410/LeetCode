13b7ab00cfc68695fbe0268040d98cf1c4f78af3
tanhao <tanhao@wondersgroup.com>
2020-12-04 10:26:38 +0800

    313. 超级丑数

--- 202012/day1204/day1204.go
+++ 202012/day1204/day1204.go
@@ -1,7 +1,33 @@
 package main
 
+import "math"
+
 func main() {
 
+}
+
+//313. 超级丑数
+func nthSuperUglyNumber(n int, primes []int) int {
+	//1是所有的数的超级丑数
+	uglyNums := []int{1}
+	primesCount := make([]int, len(primes))
+	m := make(map[int]bool)
+	for len(uglyNums) < n {
+		min := math.MaxInt32
+		minIndex := -1
+		for k2, v2 := range primes {
+			if v2*uglyNums[primesCount[k2]] < min {
+				min = v2 * uglyNums[primesCount[k2]]
+				minIndex = k2
+			}
+		}
+		if !m[min] {
+			uglyNums = append(uglyNums, min)
+			m[min] = true
+		}
+		primesCount[minIndex] += 1
+	}
+	return uglyNums[n-1]
 }
 
 //307. 区域和检索 - 数组可修改

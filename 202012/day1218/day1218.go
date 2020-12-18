81e95d18856058694f6492fa5ff15eddad629973
tanhao <tanhao@wondersgroup.com>
2020-12-18 09:32:35 +0800

    380. 常数时间插入、删除和获取随机元素

--- 202012/day1218/day1218.go
+++ 202012/day1218/day1218.go
@@ -7,17 +7,65 @@ import (
 
 func main() {
 
-	head := ListNode{
-		Val: 1,
-	}
-	head.Next = &ListNode{Val: 2}
-	head.Next.Next = &ListNode{Val: 3}
-	solution := Constructor3(&head)
+	//head := ListNode{
+	//	Val: 1,
+	//}
+	//head.Next = &ListNode{Val: 2}
+	//head.Next.Next = &ListNode{Val: 3}
+	//solution := Constructor3(&head)
 	m := []int{0, 0, 0}
+	//for i := 0; i < 10000; i++ {
+	//	m[solution.GetRandom()-1]++
+	//}
+	//fmt.Println(m)
+
+	r := Constructor2()
+	fmt.Println(r.Insert(1))
+	r.Insert(2)
+	r.Insert(3)
+	r.Insert(4)
+	fmt.Println(r.Remove(4))
+	fmt.Println(r.Remove(5))
+	fmt.Println(r.Insert(2))
 	for i := 0; i < 10000; i++ {
-		m[solution.GetRandom()-1]++
+		m[r.GetRandom()-1]++
 	}
 	fmt.Println(m)
+}
+
+//380. 常数时间插入、删除和获取随机元素
+type RandomizedSet struct {
+	//思路：采用map方式，可以方便做到常数时间删除，插入。获取随机的话，可以用蓄水池问题解决。
+	Set map[int]bool
+}
+
+func Constructor2() RandomizedSet {
+	return RandomizedSet{
+		Set: make(map[int]bool),
+	}
+}
+
+func (this *RandomizedSet) Insert(val int) bool {
+	res := this.Set[val]
+	this.Set[val] = true
+	return !res
+}
+
+func (this *RandomizedSet) Remove(val int) bool {
+	res := this.Set[val]
+	delete(this.Set, val)
+	return res
+}
+
+func (this *RandomizedSet) GetRandom() int {
+	res, n := 0, 0
+	for k, _ := range this.Set {
+		n++
+		if rand.Int()%n == 0 {
+			res = k
+		}
+	}
+	return res
 }
 
 type ListNode struct {

928216966e53c90377fba2eccfcb852004ffb990
tanhao <tanhao@wondersgroup.com>
2020-12-21 13:03:36 +0800

    427. 建立四叉树

--- 202012/day1221/day1221.go
+++ 202012/day1221/day1221.go
@@ -1,9 +1,97 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
+	fmt.Println(construct([][]int{{1, 1, 1, 1, 0, 0, 0, 0}, {1, 1, 1, 1, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 0, 0, 0, 0}, {1, 1, 1, 1, 0, 0, 0, 0}, {1, 1, 1, 1, 0, 0, 0, 0}, {1, 1, 1, 1, 0, 0, 0, 0}}))
+}
 
+type Node struct {
+	Val         bool
+	IsLeaf      bool
+	TopLeft     *Node
+	TopRight    *Node
+	BottomLeft  *Node
+	BottomRight *Node
+}
+
+//427. 建立四叉树
+func construct(grid [][]int) *Node {
+
+	//看是否都相同
+	flag := false
+	pre := grid[0][0]
+N:
+	for _, row := range grid {
+		for i := 1; i < len(row); i++ {
+			if row[i] != pre {
+				flag = true
+				break N
+			}
+		}
+	}
+
+	//有不相同的
+	if flag {
+		//需要切分，先生成一个非叶子节点
+
+		//初始化四个部分
+		topLeft := make([][]int, len(grid)/2)
+		topRight := make([][]int, len(grid)/2)
+		bottomLeft := make([][]int, len(grid)/2)
+		bottomRight := make([][]int, len(grid)/2)
+		for i := 0; i < len(grid)/2; i++ {
+			topLeft[i] = make([]int, len(grid)/2)
+			topRight[i] = make([]int, len(grid)/2)
+			bottomLeft[i] = make([]int, len(grid)/2)
+			bottomRight[i] = make([]int, len(grid)/2)
+		}
+
+		for i := 0; i < len(grid)/2; i++ {
+			for j := 0; j < len(grid)/2; j++ {
+				topLeft[i][j] = grid[i][j]
+			}
+
+			for j := len(grid) / 2; j < len(grid); j++ {
+				topRight[i][j-len(grid)/2] = grid[i][j]
+			}
+		}
+
+		for i := len(grid) / 2; i < len(grid); i++ {
+			for j := 0; j < len(grid)/2; j++ {
+				bottomLeft[i-len(grid)/2][j] = grid[i][j]
+			}
+
+			for j := len(grid) / 2; j < len(grid); j++ {
+				bottomRight[i-len(grid)/2][j-len(grid)/2] = grid[i][j]
+			}
+		}
+		return &Node{
+			Val:         true,
+			IsLeaf:      false,
+			TopLeft:     construct(topLeft),
+			TopRight:    construct(topRight),
+			BottomLeft:  construct(bottomLeft),
+			BottomRight: construct(bottomRight),
+		}
+	} else {
+		//都相同的话，直接返回一个即可
+		val := false
+		if grid[0][0] == 1 {
+			val = true
+		}
+		return &Node{
+			Val:         val,
+			IsLeaf:      true,
+			TopLeft:     nil,
+			TopRight:    nil,
+			BottomLeft:  nil,
+			BottomRight: nil,
+		}
+	}
 }
 
 //495. 提莫攻击

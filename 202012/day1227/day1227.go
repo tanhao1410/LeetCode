eea310496cd5778cf3d0d19e3f74210172f1445e
tanhao1410 <2528865013@qq.com>
2020-12-27 21:35:55 +0800

    509. 斐波那契数

--- 202012/day1227/day1227.go
+++ 202012/day1227/day1227.go
@@ -4,6 +4,19 @@ func main() {
 
 }
 
+//509. 斐波那契数
+func fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	dp := make([]int, n+1)
+	dp[1] = 1
+	for i := 2; i < n+1; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
 //520. 检测大写字母
 func detectCapitalUse(word string) bool {
 

eec35b23127334bd5ae3e6b2f9c4c110a84b4e11
tanhao <tanhao@wondersgroup.com>
2020-12-28 09:58:49 +0800

    面试题 04.05. 合法二叉搜索树

--- 202012/day1228/day1228.go
+++ 202012/day1228/day1228.go
@@ -4,6 +4,50 @@ func main() {
 
 }
 
+//每日一题：188. 买卖股票的最佳时机 IV
+func maxProfit(k int, prices []int) int {
+	//思路：1.先划分区间，每个区间都是的最小值都是开头，最大值是结尾
+	//1.1,合并部分区间，合并规则，若起点大于等于前一个的终点，应该合并
+	//2.若区间的数量小于k，则取最大的几个即可。
+	//3.若大于k,合并一部分区间，合并规则：若
+
+	return 0
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//面试题 04.05. 合法二叉搜索树
+func isValidBST(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	nums := []int{}
+	var dfs func(root *TreeNode)
+	//中序遍历，左，中，右
+	dfs = func(root *TreeNode) {
+		if root != nil {
+			dfs(root.Left)
+			nums = append(nums, root.Val)
+			dfs(root.Right)
+		}
+	}
+	dfs(root)
+
+	for pre, i := nums[0], 1; i < len(nums); i++ {
+		if nums[i] <= pre {
+			return false
+		}
+		pre = nums[i]
+	}
+
+	return true
+}
+
 //面试题 04.01. 节点间通路
 func findWhetherExistsPath(n int, graph [][]int, start int, target int) bool {
 	if n == 0 {

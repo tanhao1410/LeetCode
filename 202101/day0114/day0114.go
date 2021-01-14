ee56db4dad910747f70e38b86e80ddb729b2417c
tanhao <tanhao@wondersgroup.com>
2021-01-14 10:19:58 +0800

    537. 复数乘法

--- 202101/day0114/day0114.go
+++ 202101/day0114/day0114.go
@@ -1,7 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func main() {
 
+}
+
+//537. 复数乘法
+func complexNumberMultiply(a string, b string) string {
+	//(x+yi)*(c*di)
+	indexAPlus := strings.Index(a, "+")
+	indexAI := strings.Index(a, "i")
+	indexBPlus := strings.Index(b, "+")
+	indexBI := strings.Index(b, "i")
+	x, _ := strconv.Atoi(a[:indexAPlus])
+	y, _ := strconv.Atoi(a[indexAPlus+1 : indexAI])
+	c, _ := strconv.Atoi(b[:indexBPlus])
+	d, _ := strconv.Atoi(b[indexBPlus+1 : indexBI])
+	return fmt.Sprint((x*c - y*d), "+", (y*c + x*d), "i")
 }
 
 //每日一题：1018. 可被 5 整除的二进制前缀

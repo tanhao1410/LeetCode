17d3461fb59eba81a78396c6ad193b8d516da43a
tanhao <tanhao@wondersgroup.com>
2021-01-20 10:42:45 +0800

    622. 设计循环队列

--- 202101/day0120/day0120.go
+++ 202101/day0120/day0120.go
@@ -6,6 +6,64 @@ func main() {
 
 }
 
+//622. 设计循环队列
+type MyCircularQueue struct {
+	num []int
+	//front指向头
+	front int
+	//rear指向下一个要插入的位置，
+	rear int
+}
+
+func Constructor(k int) MyCircularQueue {
+	return MyCircularQueue{
+		num:   make([]int, k+1),
+		front: 0,
+		rear:  0,
+	}
+}
+
+func (this *MyCircularQueue) EnQueue(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+	//插入一个元素
+	this.num[this.rear] = value
+	this.rear = (this.rear - 1 + len(this.num)) % len(this.num)
+	return true
+}
+
+func (this *MyCircularQueue) DeQueue() bool {
+	if this.IsEmpty() {
+		return false
+	}
+	//移除一个数
+	this.front = (this.front - 1 + len(this.num)) % len(this.num)
+	return true
+}
+
+func (this *MyCircularQueue) Front() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	return this.num[this.front]
+}
+
+func (this *MyCircularQueue) Rear() int {
+	if this.IsEmpty() {
+		return -1
+	}
+	return this.num[(this.rear+1)%len(this.num)]
+}
+
+func (this *MyCircularQueue) IsEmpty() bool {
+	return this.front == this.rear
+}
+
+func (this *MyCircularQueue) IsFull() bool {
+	return this.front == (this.rear-1+len(this.num))%len(this.num)
+}
+
 //648. 单词替换
 func replaceWords(dictionary []string, sentence string) string {
 	res := ""

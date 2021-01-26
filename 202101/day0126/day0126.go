94690e24077ba91ee692628308ec9f321ff35b40
tanhao <tanhao@wondersgroup.com>
2021-01-26 09:14:16 +0800

    每日一题：1128. 等价多米诺骨牌对的数量

---
+++ 202101/day0126/day0126.go
@@ -0,0 +1,41 @@
+package main
+
+import "sort"
+
+func main() {
+
+}
+
+//每日一题：1128. 等价多米诺骨牌对的数量
+func numEquivDominoPairs(dominoes [][]int) int {
+	//可以由数组改为大的*10 + 小的，相等的就是可替换的。问题就变成了普通的数组，就相等的数的数量了。
+	nums := make([]int, len(dominoes))
+	for k, dominoe := range dominoes {
+		if dominoe[0] > dominoe[1] {
+			nums[k] = dominoe[0]*10 + dominoe[1]
+		} else {
+			nums[k] = dominoe[1]*10 + dominoe[0]
+		}
+	}
+
+	//排序下
+	sort.Ints(nums)
+	res := 0
+	pre := nums[0]
+	count := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == pre {
+			count++
+			//最后一个数特殊处理
+			if i == len(nums)-1 {
+				res += (count * (count - 1) / 2)
+			}
+		} else {
+			res += (count * (count - 1) / 2)
+			count = 1
+			pre = nums[i]
+		}
+	}
+
+	return res
+}

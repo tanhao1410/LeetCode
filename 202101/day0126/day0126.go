4fb4f139f3f4044448d3409039877ed94034fca2
tanhao <tanhao@wondersgroup.com>
2021-01-26 09:30:01 +0800

    面试题 08.04. 幂集

--- 202101/day0126/day0126.go
+++ 202101/day0126/day0126.go
@@ -6,6 +6,19 @@ func main() {
 
 }
 
+//面试题 08.04. 幂集
+func subsets(nums []int) [][]int {
+	res := [][]int{[]int{}}
+	//先选一个数
+	for i := 0; i < len(nums); i++ {
+		re := subsets(nums[i+1:])
+		for _, v := range re {
+			res = append(res, append(v, nums[i]))
+		}
+	}
+	return res
+}
+
 //每日一题：1128. 等价多米诺骨牌对的数量
 func numEquivDominoPairs(dominoes [][]int) int {
 	//可以由数组改为大的*10 + 小的，相等的就是可替换的。问题就变成了普通的数组，就相等的数的数量了。

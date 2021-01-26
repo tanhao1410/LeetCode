105d988a3f2af84560d31e454a51c4ef7a275bfa
tanhao <tanhao@wondersgroup.com>
2021-01-26 09:50:13 +0800

    面试题 16.17. 连续数列

--- 202101/day0126/day0126.go
+++ 202101/day0126/day0126.go
@@ -6,6 +6,23 @@ func main() {
 
 }
 
+//面试题 16.17. 连续数列
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	res := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] > 0 {
+			nums[i] = nums[i-1] + nums[i]
+		}
+		if nums[i] > res {
+			res = nums[i]
+		}
+	}
+	return res
+}
+
 //面试题 08.04. 幂集
 func subsets(nums []int) [][]int {
 	res := [][]int{[]int{}}

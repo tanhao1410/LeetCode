25ad0d8b4aec38f9d9e4ea91fd2bc23086d70753
tanhao <tanhao@wondersgroup.com>
2021-02-01 09:45:35 +0800

    面试题 16.25. LRU 缓存

--- 202102/day0201/day0201.go
+++ 202102/day0201/day0201.go
@@ -1,7 +1,73 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	cache := Constructor(2)
+	cache.Put(1, 1)
+	cache.Put(2, 2)
+	cache.Put(3, 3)
+	fmt.Println(cache.Get(1))
+}
+
+//面试题 16.25. LRU 缓存
+type LRUCache struct {
+	//存储数
+	m map[int]int
+	//访问序列
+	seq []int
+	//容量
+	capacity int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		m:        make(map[int]int),
+		seq:      []int{},
+		capacity: capacity,
+	}
+}
+
+func (this *LRUCache) Get(key int) int {
+	if res, ok := this.m[key]; ok {
+		//会更新索引
+		next := this.seq[0]
+		for i := 0; i < len(this.seq) && next != key; i++ {
+			//往前走一位
+			this.seq[i+1], next = next, this.seq[i+1]
+		}
+		this.seq[0] = key
+		return res
+	}
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+
+	//看key是否存在
+	if this.Get(key) != -1 {
+		//更新值
+		this.m[key] = value
+		//更新seq 即可。
+		next := this.seq[0]
+		for i := 0; i < len(this.seq) && next != key; i++ {
+			//往前走一位
+			this.seq[i+1], next = next, this.seq[i+1]
+		}
+		this.seq[0] = key
+		return
+	}
+
+	//思路：如果大于容量，要删除一个
+	if len(this.m) < this.capacity {
+		this.seq = append([]int{key}, this.seq...)
+	} else {
+		//需要删除一个，删除哪一个？删除seq中的最后一项即可
+		delete(this.m, this.seq[len(this.seq)-1])
+		this.seq = this.seq[:len(this.seq)-1]
+		this.seq = append([]int{key}, this.seq...)
+	}
+	this.m[key] = value
 }
 
 //每日一题：888. 公平的糖果棒交换

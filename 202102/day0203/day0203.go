28ddd74b78ac0d785b0c533d244ccde22db37b7f
tanhao <tanhao@wondersgroup.com>
2021-02-03 14:19:51 +0800

    268. 丢失的数字

--- 202102/day0203/day0203.go
+++ 202102/day0203/day0203.go
@@ -1,6 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(missingNumber([]int{1, 0, 3}))
+}
+
+//268. 丢失的数字
+func missingNumber(nums []int) int {
+	//最简单思路：空间复杂度o(n) ，或者排序完成。
+	temp := make([]bool, len(nums)+1)
+	for i := 0; i < len(nums); i++ {
+		temp[nums[i]] = true
+	}
+
+	for i := 0; ; i++ {
+		if !temp[i] {
+			return i
+		}
+	}
 
 }
 

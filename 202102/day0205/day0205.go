b91862535c0922f42ad83f486db391f3b9adb3ae
tanhao <tanhao@wondersgroup.com>
2021-02-05 11:21:27 +0800

    581. 最短无序连续子数组

--- 202102/day0205/day0205.go
+++ 202102/day0205/day0205.go
@@ -6,7 +6,56 @@ import (
 
 func main() {
 	fmt.Println(equalSubstring("krpgjbjjznpzdfy", "nxargkbydxmsgby", 14))
+	fmt.Println(findUnsortedSubarray([]int{2, 6, 4, 8, 10, 9, 15}))
+}
 
+//581. 最短无序连续子数组
+func findUnsortedSubarray(nums []int) int {
+	//它前面最大的数，包括自身
+	max := make([]int, len(nums))
+	//它后面最小的数
+	min := make([]int, len(nums))
+
+	//子数组的开头应为：它前面的最大的数 要比后面最小的数 要 小
+	for i := 0; i < len(nums); i++ {
+		if i == 0 {
+			max[i] = nums[i]
+		} else {
+			if max[i-1] < nums[i] {
+				max[i] = nums[i]
+			} else {
+				max[i] = max[i-1]
+			}
+		}
+	}
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if i == len(nums)-1 {
+			min[i] = nums[i]
+		} else {
+			if min[i+1] < nums[i] {
+				min[i] = min[i+1]
+			} else {
+				min[i] = nums[i]
+			}
+		}
+	}
+
+	//找连续子数组的开头，即 不满足后面最小的要比前面最大还要大
+	start := 0
+	for ; start < len(nums) && max[start] <= min[start]; start++ {
+	}
+
+	//找连续子数组的结尾 即 不满足 前面最大的比后面最小的还要大
+	end := len(nums) - 1
+	for ; end >= 0 && max[end] <= min[end]; end-- {
+	}
+
+	if start > end {
+		return 0
+	}
+
+	return end - start + 1
 }
 
 //1539. 第 k 个缺失的正整数

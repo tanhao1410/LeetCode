0373b0714358f5c94bfb2154591d23a2d64bc0ff
tanhao <tanhao@wondersgroup.com>
2021-03-16 10:58:22 +0800

    面试题 05.08. 绘制直线

--- 202103/day0316/day0316.go
+++ 202103/day0316/day0316.go
@@ -1,7 +1,38 @@
 package main
 
+import "fmt"
+
 func main() {
 	//
+	fmt.Println(drawLine(6, 96, 0, 95, 1))
+}
+
+//面试题 05.08. 绘制直线
+func drawLine(length int, w int, x1 int, x2 int, y int) []int {
+	res := make([]int, length)
+	//从哪一位开始
+	start := y * (w / 32)
+	//先组建显示行的数 字节数组
+	bytes := make([]int, w)
+	for i := x1; i <= x2; i++ {
+		bytes[i] = 1
+	}
+
+	createNum := func(index int) int {
+		var res int32 = 0
+		for i := 0; i < 32; i++ {
+			res <<= 1
+			res += int32(1 & bytes[index+i])
+		}
+		return int(res)
+	}
+
+	//形成数
+	for i := 0; i < w; i += 32 {
+		res[start+i/32] = createNum(i)
+	}
+
+	return res
 }
 
 //面试题 05.06. 整数转换

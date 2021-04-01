d1379fe5bd5f0cd4ad1dfe085cc0f3fe26fc47f9
tanhao <tanhao@wondersgroup.com>
2021-04-01 10:16:31 +0800

    1472. 设计浏览器历史记录

--- 202104/day0401/day0401.go
+++ 202104/day0401/day0401.go
@@ -4,6 +4,57 @@ func main() {
 
 }
 
+//1472. 设计浏览器历史记录
+type BrowserHistory struct {
+	Pages []string
+	Cur   int
+	Last  int
+}
+
+func Constructor(homepage string) BrowserHistory {
+	return BrowserHistory{
+		Pages: []string{homepage},
+	}
+}
+
+func (this *BrowserHistory) Visit(url string) {
+	if this.Last == this.Cur {
+
+		//原有的满了
+		if this.Last == len(this.Pages)-1 {
+			this.Pages = append(this.Pages, url)
+			this.Cur++
+			this.Last++
+		} else {
+			this.Cur++
+			this.Last++
+			this.Pages[this.Cur] = url
+		}
+	} else {
+		this.Cur++
+		this.Pages[this.Cur] = url
+		this.Last = this.Cur
+	}
+}
+
+func (this *BrowserHistory) Back(steps int) string {
+	if this.Cur-steps <= 0 {
+		//回退到
+		this.Cur = 0
+		return this.Pages[0]
+	}
+	this.Cur -= steps
+	return this.Pages[this.Cur]
+}
+
+func (this *BrowserHistory) Forward(steps int) string {
+	this.Cur += steps
+	if this.Cur > this.Last {
+		this.Cur = this.Last
+	}
+	return this.Pages[this.Cur]
+}
+
 //每日一题：1006. 笨阶乘
 func clumsy(N int) int {
 	//思路：按照*/+-顺序来

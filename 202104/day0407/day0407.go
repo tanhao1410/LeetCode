c3f5f1f8a098e3e962f95a553b74ff6efdad3850
tanhao <tanhao@wondersgroup.com>
2021-04-07 14:29:28 +0800

    474. 一和零

--- 202104/day0407/day0407.go
+++ 202104/day0407/day0407.go
@@ -1,7 +1,71 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(findMaxForm([]string{"10", "0001", "111001", "1", "0"}, 4, 3))
+}
+
+//474. 一和零
+func findMaxForm(strs []string, m int, n int) int {
+	//dp[i][j][k] 0..i个字符串 需要 j个0，k 个 1
+
+	//当前字符串可以选择，可以不选择
+	//dp[i][j][k] = dp[i - 1][j][k] //不选择当前字符串
+	//dp[i][j][k] = dp[i-1][j-zeroCount][k-oneCount]
+	//选择其中比较大的。
+
+	//dp[0][0][0] = 0;
+	dp := make([][][]int, len(strs))
+	for i := 0; i < len(strs); i++ {
+		dp[i] = make([][]int, m+1)
+		for j := 0; j < m+1; j++ {
+			dp[i][j] = make([]int, n+1)
+		}
+	}
+
+	countNum := func(num int, str string) int {
+		zero, one := 0, 0
+		for _, v := range str {
+			if v == '1' {
+				one++
+			} else {
+				zero++
+			}
+		}
+		if num == 1 {
+			return one
+		}
+		return zero
+	}
+
+	for i := 0; i < len(strs); i++ {
+		for j := 0; j < m+1; j++ {
+			for k := 0; k < n+1; k++ {
+
+				if i == 0 {
+					//第一个字符串能满足最多m个0，n个1的情况下，最大返回也就是1，不满足就是0
+					if countNum(0, strs[0]) <= j && countNum(1, strs[0]) <= k {
+						dp[0][j][k] = 1
+					}
+				} else if i > 0 {
+					//可以选择当前字符串,则它的前面需要的0，1要满足条件
+					if j-countNum(0, strs[i]) >= 0 && k-countNum(1, strs[i]) >= 0 {
+						if 1+dp[i-1][j-countNum(0, strs[i])][k-countNum(1, strs[i])] > dp[i-1][j][k] {
+							dp[i][j][k] = 1 + dp[i-1][j-countNum(0, strs[i])][k-countNum(1, strs[i])]
+						} else {
+							dp[i][j][k] = dp[i-1][j][k]
+						}
+					} else {
+						//不选择当前字符串
+						dp[i][j][k] = dp[i-1][j][k]
+					}
+				}
+			}
+		}
+	}
+
+	return dp[len(strs)-1][m][n]
 }
 
 //258. 各位相加

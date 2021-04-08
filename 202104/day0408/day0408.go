f16f7381ba21f0f8ac86d23e692b1f2d0f39e2e3
tanhao <tanhao@wondersgroup.com>
2021-04-08 10:07:29 +0800

    322. 零钱兑换

--- 202104/day0408/day0408.go
+++ 202104/day0408/day0408.go
@@ -14,6 +14,50 @@ func main() {
 	}
 }
 
+//322. 零钱兑换
+func coinChange(coins []int, amount int) int {
+	//背包问题： 最少硬币个数
+	//dp[i][j] 凑成j金额所需要的最少数量 i 为coins
+	// dp[i + 1][j] =
+	//1.不用这个金额 dp[i + 1][j] = dp[i][j]
+	//2.用这个金额 dp[i+1][j] = dp[i][j-coins[i+1] * n] + n 关键问题，n 应该怎么办？ 找到它的最小值
+	//还需要考虑总数
+	dp := make([][]int, len(coins))
+	for i := 0; i < len(coins); i++ {
+		dp[i] = make([]int, amount+1)
+	}
+	for i := 0; i < len(coins); i++ {
+		for j := 0; j < amount+1; j++ {
+			//就一种硬币时，直接看是否能整除即可
+			if i == 0 {
+				if j%coins[i] == 0 {
+					dp[i][j] = j / coins[i]
+				} else {
+					dp[i][j] = -1
+				}
+			} else {
+				//多了一种硬币后，可以选择用新硬币的数量 假设为n
+				min := 100000
+				for n := 0; n*coins[i] <= j; n++ {
+					if dp[i-1][j-coins[i]*n] != -1 { //说明前面可以凑成
+						//能凑成，但不一定是最小的。
+						if dp[i-1][j-coins[i]*n]+n < min {
+							min = dp[i-1][j-coins[i]*n] + n
+						}
+					}
+				}
+
+				if min == 100000 {
+					dp[i][j] = -1
+				} else {
+					dp[i][j] = min
+				}
+			}
+		}
+	}
+	return dp[len(coins)-1][amount]
+}
+
 //18. 四数之和
 func fourSum(nums []int, target int) [][]int {
 	// 先排序，从小到大

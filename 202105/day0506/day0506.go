527b9fd190db6a8f662597109c0d646976d66b39
tanhao <tanhao@wondersgroup.com>
2021-05-06 10:29:32 +0800

    剑指 Offer 59 - II. 队列的最大值

--- 202105/day0506/day0506.go
+++ 202105/day0506/day0506.go
@@ -13,3 +13,77 @@ func decode(encoded []int, first int) []int {
 	}
 	return res
 }
+
+//剑指 Offer 59 - II. 队列的最大值
+type Node struct {
+	Val  int
+	Next *Node
+}
+
+type MaxQueue struct {
+	MaxNode *Node
+	Head    *Node
+	Tail    *Node
+}
+
+func Constructor() MaxQueue {
+	return MaxQueue{}
+}
+
+func (this *MaxQueue) Max_value() int {
+	if this.MaxNode == nil {
+		return -1
+	}
+	return this.MaxNode.Val
+}
+
+func (this *MaxQueue) Push_back(value int) {
+	newNode := &Node{
+		Val: value,
+	}
+	//没有节点的时候
+	if this.Tail == nil {
+		this.Tail = newNode
+		this.Head = this.Tail
+		this.MaxNode = this.Tail
+		return
+	}
+	//有节点，新节点插入到尾后面，更新尾
+	this.Tail.Next = newNode
+	this.Tail = newNode
+
+	//如果比最大值大，更新最大值节点
+	if value >= this.MaxNode.Val {
+		this.MaxNode = this.Tail
+	}
+
+	return
+}
+
+func (this *MaxQueue) Pop_front() int {
+
+	if this.Head == nil {
+		return -1
+	}
+	res := this.Head.Val
+
+	//如果就一个，直接返回即可
+	if this.Head == this.Tail {
+		this.Head = nil
+		this.Tail = nil
+		this.MaxNode = nil
+		return res
+	} else if this.Head == this.MaxNode {
+		//寻找新的最大值
+		maxNode := this.Head.Next
+		for p := maxNode.Next; p != nil; p = p.Next {
+			if p.Val > maxNode.Val {
+				maxNode = p
+			}
+		}
+		this.MaxNode = maxNode
+	}
+
+	this.Head = this.Head.Next
+	return res
+}

d7ddd3774468b6d3e967dffd210d6eef95dd2820
tanhao <tanhao@wondersgroup.com>
2021-05-13 15:37:29 +0800

    238. 除自身以外数组的乘积

--- 202105/day0513/src/main.rs
+++ 202105/day0513/src/main.rs
@@ -5,6 +5,30 @@ fn main() {
 pub struct Solution {}
 
 impl Solution {
+
+    //238. 除自身以外数组的乘积
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        let mut res = vec![1;nums.len()];
+
+        (1..nums.len()).for_each(|i|{
+            res[i] = res[i-1] * nums[i];
+        });
+
+        let mut temp = res[res.len() - 1];
+        (0..nums.len() - 1).rev().for_each(|i|{
+            res[i] *= temp;
+            temp *= nums[i];
+        });
+
+        res
+    }
+
+    //238. 除自身以外数组的乘积-使用除法
+    pub fn product_except_self2(nums: Vec<i32>) -> Vec<i32> {
+        let sum = nums.iter().fold(1,|p,q| p * q);
+        nums.iter().map(|i| sum / i).collect()
+    }
+
     //279. 完全平方数
     pub fn num_squares(n: i32) -> i32 {
         //先得到所有的完全平方数

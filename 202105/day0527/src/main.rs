c9a2de027b9511f3c50ebfdc2e0857d33fc19c51
tanhao <tanhao@wondersgroup.com>
2021-05-27 14:01:56 +0800

    384. 打乱数组

--- 202105/day0527/src/main.rs
+++ 202105/day0527/src/main.rs
@@ -2,6 +2,48 @@ fn main() {
     println!("Hello, world!");
 }
 
+//384. 打乱数组
+struct Solution {
+    nums: Vec<i32>
+}
+
+impl Solution {
+    fn new(nums: Vec<i32>) -> Self {
+        Solution {
+            nums
+        }
+    }
+
+    fn reset(&self) -> Vec<i32> {
+        self.nums.clone()
+    }
+
+    fn rand(&self, max: usize) -> usize {
+        //std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).ok().unwrap().as_millis() % usize
+        let mut rand_num = 0;
+        unsafe {
+            std::arch::x86_64::_rdrand16_step(&mut rand_num);
+        }
+        rand_num as usize % max
+    }
+
+    fn shuffle(&self) -> Vec<i32> {
+        //随机进行
+        let mut res = vec![];
+        let mut temp = self.nums.clone();
+        for i in (1..self.nums.len() + 1).rev() {
+            let index = self.rand(i);
+            // let last_num = temp[i - 1];
+            // res.push(std::mem::replace(&mut temp[index],last_num));
+            unsafe {
+                std::ptr::swap(&mut temp[i - 1], &mut temp[index]);
+            }
+            res.push(temp[i - 1]);
+        }
+        res
+    }
+}
+
 //371. 两整数之和
 pub fn get_sum(a: i32, b: i32) -> i32 {
     //从最低位开始，&，^,

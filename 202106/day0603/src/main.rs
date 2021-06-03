777da1a66d21bd6c435a3105863b2856cc908852
tanhao <tanhao@wondersgroup.com>
2021-06-03 13:32:26 +0800

    1160. 拼写单词

--- 202106/day0603/src/main.rs
+++ 202106/day0603/src/main.rs
@@ -2,6 +2,23 @@ fn main() {
     println!("Hello, world!");
 }
 
+//1160. 拼写单词
+pub fn count_characters(words: Vec<String>, chars: String) -> i32 {
+    let letter_count = |s: &String| -> std::collections::HashMap<char, i32>{
+        s.chars().fold(std::collections::HashMap::new(), |mut m, c| {
+            *m.entry(c).or_insert(0) += 1;
+            m
+        })
+    };
+    //思路：反向思维，从单词反向出发，看该单词是否能有chars中的字符拼成
+    let m = letter_count(&chars);
+    words.iter().filter(|&w| {
+        letter_count(w).iter().fold(true, |p, (&w, &c)| {
+            p && m.contains_key(&w) && m.get(&w).unwrap() >= &c
+        })
+    }).map(|s| s.len()).sum()
+}
+
 //525. 连续数组
 pub fn find_max_length(nums: Vec<i32>) -> i32 {
     //记录1与0数量差 最靠前的位置

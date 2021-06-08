774f0f7621430c8edd6e7946d266c1f7dfaddb30
tanhao <tanhao@wondersgroup.com>
2021-06-08 12:47:32 +0800

    239. 滑动窗口最大值

---
+++ 202106/day0608/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//239. 滑动窗口最大值
+pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    //思路：第一次的时候，找到最大值及所在的位置 k
+    //每往前移动一个，带入一个新的数字，若该数字小于最大值， 最大值没有被踢掉，则依旧为原最大值
+    //若数字小于最大值，最大值被踢掉 ？重新寻找最大值。
+    //采用TreeMap
+    #![feature(map_first_last)]
+    use std::collections::BTreeMap;
+    let mut tree_map = BTreeMap::new();
+    //初始化TreeMap
+    for i in 0..k as usize {
+        *tree_map.entry(nums[i]).or_insert(0) += 1;
+    }
+    let mut res = vec![*tree_map.last_entry().unwrap().key()];
+
+    for i in k as usize..nums.len() {
+        //进入一个值，出来一个值
+        let remove = nums[i - k as usize];
+        let come = nums[i];
+        *tree_map.entry(remove).or_insert(0) -= 1;
+        if *tree_map.get(&remove).unwrap() < 1{
+            tree_map.remove(&remove);
+        }
+        *tree_map.entry(come).or_insert(0) += 1;
+        res.push(*tree_map.last_entry().unwrap().key())
+    }
+    res
+}

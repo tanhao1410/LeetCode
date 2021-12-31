70fe9400467aab475b6266769b3002b765249c92
tanhao1410 <2528865013@qq.com>
2021-12-31 17:09:36 +0800

    689. 三个无重叠子数组的最大和

--- 202112/supplement/src/main.rs
+++ 202112/supplement/src/main.rs
@@ -40,4 +40,53 @@ impl Solution {
             .filter(i32::is_positive)
             .sum()
     }
+
+    //689. 三个无重叠子数组的最大和
+    pub fn max_sum_of_three_subarrays(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        //思路：可以先简化问题，当只要一个无重叠时，从后往前，以每一个位置为起始的数组的最大和易求
+        //当上升到两个数组时，从后往前，开始计算第一个数组的
+        let (mut v1,mut v2,mut v3) = (vec![0;nums.len()],vec![0;nums.len()],vec![0;nums.len()]);
+        //记录切割字符串时的下标
+        let (mut l1,mut l2,mut l3) = (vec![0;nums.len()],vec![0;nums.len()],vec![0;nums.len()]);
+        //先计算只划分一个数组情况
+        v1[nums.len() - k as usize] = nums.iter().rev().take(k as usize).sum::<i32>();
+        l1[nums.len() - k as usize] = nums.len() - k as usize;
+        for i in (2 * k as usize ..nums.len() - k as usize).rev(){
+            let sum = nums.iter().skip(i).take(k as usize).sum::<i32>();
+            if sum >= v1[i + 1]{
+                v1[i] = sum;
+                l1[i] = i;
+            }else{
+                v1[i] = v1[i + 1];
+                l1[i] = l1[i + 1];
+            }
+        }
+        //计算当划分为两个数组时
+        v2[nums.len() - 2 * k as usize] = nums.iter().skip(nums.len() - 2 * k as usize).take(k as usize).sum::<i32>() + v1[nums.len() - k as usize];
+        l2[nums.len() - 2 * k as usize] = nums.len() - 2 * k as usize;
+        for i in (k as usize..nums.len() - 2 * k as usize).rev(){
+            let sum = nums.iter().skip(i).take(k as usize).sum::<i32>() + v1[i + k as usize];
+            if sum >= v2[i + 1]{
+                v2[i] = sum;
+                l2[i] = i;
+            }else{
+                v2[i] = v2[i + 1];
+                l2[i] = l2[i + 1];
+            }
+        }
+        //计算当分为三个数组时
+        v3[nums.len() - 3 * k as usize] = nums.iter().skip(nums.len() - 3 * k as usize).take(k as usize).sum::<i32>() + v2[nums.len() - 2 * k as usize];
+        l3[nums.len() - 3 * k as usize] = nums.len() - 3 * k as usize;
+        for i in (0..nums.len() - 3 * k as usize).rev(){
+            let sum = nums.iter().skip(i).take(k as usize).sum::<i32>() + v2[i + k as usize];
+            if sum >= v3[i + 1]{
+                v3[i] = sum;
+                l3[i] = i;
+            }else{
+                v3[i] = v3[i + 1];
+                l3[i] = l3[i + 1];
+            }
+        }
+        vec![l3[0] as i32,l2[l3[0] + k as usize] as i32,l1[l2[l3[0] + k as usize] + k as usize] as i32]
+    }
 }

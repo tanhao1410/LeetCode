238b4419030ad8132b5fcdfcba836d0d92fa9651
tanhao1410 <2528865013@qq.com>
2021-12-31 17:07:55 +0800

    825. 适龄的朋友

--- 202112/supplement/src/main.rs
+++ 202112/supplement/src/main.rs
@@ -14,4 +14,30 @@ impl Solution {
             .sum::<i32>()
             == 2 * num
     }
+
+    //825. 适龄的朋友
+    pub fn num_friend_requests(ages: Vec<i32>) -> i32 {
+        let mut ages_num = vec![0; 121];
+        ages.iter().for_each(|&age| {
+            ages_num[age as usize] += 1;
+        });
+        //判断x是否向y发送消息
+        let is_send_to = |x, y| -> bool{
+            x >= y && y > x / 2 + 7 && (y <= 100 || x >= 100)
+        };
+        //年龄为x的所有人，发送的消息量
+        let age_send_count = |x| -> i32{
+            (1..x + 1).filter(|&y| is_send_to(x, y))
+                .map(|y| ages_num[y])
+                .sum::<i32>()
+                * ages_num[x]
+                - ages_num[x]
+        };
+        (1..121)
+            .map(|i|ages_num[i])
+            .filter(&i32::is_positive)
+            .map(age_send_count)
+            .filter(i32::is_positive)
+            .sum()
+    }
 }

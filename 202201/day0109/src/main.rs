61be47c7a24b2bb534739b4eae28c987a890f0e7
tanhao1410 <2528865013@qq.com>
2022-01-09 21:38:49 +0800

    1629. 按键持续时间最长的键

--- 202201/day0109/src/main.rs
+++ 202201/day0109/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, world!");
 }
@@ -5,6 +7,7 @@ fn main() {
 struct Solution {}
 
 impl Solution {
+
     pub fn slowest_key(release_times: Vec<i32>, keys_pressed: String) -> char {
         keys_pressed
             .chars()
@@ -13,4 +16,21 @@ impl Solution {
             .fold((0, 'a'), |r, e| if e.0 > r.0 || (e.0 == r.0 && e.1 > r.1) { e } else { r })
             .1
     }
+
+    //参考python简洁解法思路：max(zip(map(int.__sub__,releaseTimes,[0]+releaseTimes),keysPressed))[1]
+    pub fn slowest_key2(release_times: Vec<i32>, keys_pressed: String) -> char {
+        vec![0]
+            .iter()
+            .chain(release_times.iter())
+            .zip(release_times.iter())
+            .map(|(p,q)| q - p)
+            .zip(keys_pressed.chars())
+            .max_by(|h,t|match (h.0.cmp(&t.0),h.1.cmp(&t.1)){
+                (Ordering::Greater,_)|(Ordering::Equal,Ordering::Greater) => Ordering::Greater,
+                _=>Ordering::Less
+            })
+            .unwrap()
+            .1
+    }
+
 }

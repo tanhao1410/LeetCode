710ecf61d81b2b234f7d645d2e9c48a90f58d5a3
tanhao1410 <2528865013@qq.com>
2022-01-15 16:57:35 +0800

    6. Z 字形变换

--- 202201/day0115/src/main.rs
+++ 202201/day0115/src/main.rs
@@ -9,3 +9,71 @@ pub fn total_money(n: i32) -> i32 {
         mod_ => mod_,
     }).sum()
 }
+
+//6. Z 字形变换
+pub fn convert2(s: String, num_rows: i32) -> String {
+    if num_rows == 1{
+        return s;
+    }
+
+    let mut matrix = vec![vec![];num_rows as usize];
+    let mut row = 0;
+    let mut is_down = true;
+    for c in s.chars(){
+        matrix[row].push(c);
+        if is_down{
+            if row == num_rows as usize - 1{
+                is_down = false;
+                row -= 1;
+            }else{
+                row += 1;
+            }
+        }else{
+            if row == 0{
+                is_down = true;
+                row += 1;
+            }else{
+                row -= 1;
+            }
+        }
+    }
+    matrix
+        .iter()
+        .flat_map(|chars|chars.iter())
+        .collect()
+}
+
+pub fn convert(s: String, num_rows: i32) -> String {
+    if num_rows == 1{
+        return s;
+    }
+
+    let mut matrix = vec![vec![];num_rows as usize];
+    let mut row = 0;
+    let mut is_down = true;
+    for c in s.chars(){
+        matrix[row].push(c);
+        if is_down{
+            if row + 1 == num_rows as usize{
+                is_down = false;
+                row -= 1;
+            }else{
+                row += 1;
+            }
+        }else{
+            if row == 0{
+                is_down = true;
+                row += 1;
+            }else{
+                row -= 1;
+            }
+        }
+    }
+    let mut res = String::with_capacity(s.len());
+    for chars in matrix.iter(){
+        for char in chars.iter(){
+            res.push(*char);
+        }
+    }
+    res
+}

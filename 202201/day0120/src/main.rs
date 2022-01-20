b565a2cd02d5bef1d2f3ec801d35b63853ddcdf6
tanhao1410 <2528865013@qq.com>
2022-01-20 09:57:54 +0800

    213. 打家劫舍 II

--- 202201/day0120/src/main.rs
+++ 202201/day0120/src/main.rs
@@ -1,5 +1,30 @@
+use std::arch::x86_64::_mm256_mask_compress_epi32;
+
 fn main() {
     println!("Hello, world!");
+}
+
+//213. 打家劫舍 II
+pub fn rob2(nums: Vec<i32>) -> i32 {
+    //区别在于是环形的 dp[i] = max{dp[i-2] + nums[i],dp[i - 1]} 限制是，dp[0] 与dp[len - 1]不可共存
+    //思路1：转化为198，dp[n-1] 或  去除掉nums[0] 后，求dp[n-1]。大的为返回结果。
+    let rob = |nums: &[i32]| {
+        if nums.len() < 2 {
+            return nums[0];
+        }
+        let mut pre1 = nums[0];
+        let mut pre2 = nums[1];
+        for i in 2..nums.len() {
+            let temp = pre2.max(pre1 + nums[i]);
+            pre1 = pre2.max(pre1);
+            pre2 = temp;
+        }
+        pre1.max(pre2)
+    };
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    rob(&nums[1..]).max(rob(&nums[..nums.len() - 1]))
 }
 
 //198. 打家劫舍

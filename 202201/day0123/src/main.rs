923ba63795193d124bc2345cb1c946f03881fba0
tanhao1410 <2528865013@qq.com>
2022-01-23 10:15:18 +0800

    152. 乘积最大子数组

---
+++ 202201/day0123/src/main.rs
@@ -0,0 +1,55 @@
+fn main() {
+    println!("Hello, world!");
+
+    assert_eq!(max_product(vec![0,2]),2);
+    assert_eq!(max_product(vec![2,-5,-2,-4,3]),24);
+    assert_eq!(max_product(vec![-2,3,-4]),24);
+    assert_eq!(max_product(vec! [-2,0,-1]),0);
+    assert_eq!(max_product(vec! [2,3,-2,4]),6);
+}
+
+//152. 乘积最大子数组
+pub fn max_product(nums: Vec<i32>) ->  i32 {
+    let mut dp_max = vec![0; nums.len()]; //以nums[i]结尾的最大子数组之积
+    let mut dp_min = vec![0; nums.len()]; //以nums[i]结尾的负的最小值。
+
+    dp_max[0] = nums[0];
+    if nums[0] < 0{
+        dp_min[0] = nums[0];
+    }
+
+    for i in 1..nums.len() {
+        if nums[i] >= 0 {
+            //看它的最大值是否大于0，如果大于0，则dp_max[i] = nums[i] * dp[i - 1]
+            //如果它的前一个最大值小于0，自己又大于0，所以，dp_max[i] = nums[i]
+            //如果它的前一个等于0，自己又大于0，dp_max[i] = nums[i]
+            //更新dp_min
+            //dp_min[i] = nums[i] * dp_min[i - 1]，因为自己大于0，而前面的最大负，乘上正数，肯定更小
+            if dp_max[i - 1] > 0 {
+                dp_max[i] = nums[i] * dp_max[i - 1];
+            } else {
+                dp_max[i] = nums[i];
+            }
+            dp_min[i] = nums[i] * dp_min[i - 1];
+        } else {
+            //如果当前值小于0
+            //如果dp_max[i - 1] > 0,dp_min[i - 1] < 0,=>dp_max[i] = nums[i] * dp_min[i - 1]
+            //如果dp_max[i - 1] > 0,_ =>dp_max[i] = nums[i],
+            //
+            if dp_max[i-1] > 0{
+                if dp_min[i - 1] < 0{
+                    //更新最大值
+                    dp_max[i] = nums[i] * dp_min[i - 1];
+                }else{
+                    dp_max[i] = nums[i];
+                }
+                //更新负的最小值
+                dp_min[i] = nums[i] * dp_max[i - 1];
+            }else{
+                dp_max[i] = dp_max[i - 1].min(dp_min[i - 1]) * nums[i];
+                dp_min[i] = nums[i];
+            }
+        }
+    }
+    dp_max.into_iter().max().unwrap()
+}

32b40fd3e98b0e3d24d57ae354c9f17dc27805c5
tanhao1410 <2528865013@qq.com>
2022-01-24 10:25:28 +0800

    1014. 最佳观光组合

--- 202201/day0124/src/main.rs
+++ 202201/day0124/src/main.rs
@@ -2,6 +2,23 @@ fn main() {
     println!("Hello, world!");
 }
 
+//1014. 最佳观光组合
+pub fn max_score_sightseeing_pair(values: Vec<i32>) -> i32 {
+    //思路:dp[i]以values[i]结尾的组合，最大的值
+    //dp[i] = dp[i-1] - 1 - values[i - 1] + values[i] 或 values[i] + values[i -1] - 1
+    let mut dp = values[0] + values[1] - 1;
+    let mut res = dp;
+    for i in 2..values.len(){
+        if dp > 2 * values[i - 1]{
+            dp = dp - 1 - values[i - 1] + values[i];
+        }else{
+            dp = values[i - 1] - 1 + values[i];
+        }
+        res = res.max(dp);
+    }
+    res
+}
+
 //557. 反转字符串中的单词 III
 pub fn reverse_words(s: String) -> String {
     s

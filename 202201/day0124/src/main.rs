540982165e33fbc570713a0d6f3137040143a50f
tanhao1410 <2528865013@qq.com>
2022-01-24 10:14:43 +0800

    557. 反转字符串中的单词 III

--- 202201/day0124/src/main.rs
+++ 202201/day0124/src/main.rs
@@ -2,11 +2,20 @@ fn main() {
     println!("Hello, world!");
 }
 
+//557. 反转字符串中的单词 III
+pub fn reverse_words(s: String) -> String {
+    s
+        .split(" ")
+        .map(|s| s.chars().rev().collect::<String>())
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
 //519. 随机翻转矩阵
 pub fn reverse_string(s: &mut Vec<char>) {
     let mut i = 0;
     let mut j = s.len() - 1;
-    while j > i{
+    while j > i {
         let temp = s[i];
         s[i] = s[j];
         s[j] = temp;

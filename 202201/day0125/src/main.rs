529f381fa303c07f7f62c427daa5ab4a42ad6d3d
tanhao1410 <2528865013@qq.com>
2022-01-25 10:38:37 +0800

    19. 删除链表的倒数第 N 个结点

--- 202201/day0125/src/main.rs
+++ 202201/day0125/src/main.rs
@@ -26,6 +26,35 @@ impl ListNode {
 struct Solution;
 
 impl Solution {
+    //19. 删除链表的倒数第 N 个结点
+    pub fn remove_nth_from_end(mut head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        //最原始思路，遍历一遍，得到链表长度。如何一次遍历即可呢。双指针，一个在前，一个在后，在前的先走n步，
+        let mut fast = head.as_ref().unwrap();
+        let mut slow = head.as_ref().unwrap();
+        //快的先走 n步，然后，慢的指向的就是要删的。
+        for _ in 0..n {
+            if let Some(next) = fast.next.as_ref() {
+                fast = next;
+            } else {
+                //说明走到头了，即删除的是第一个元素。或长度本来就是1
+                return head.unwrap().next.take();
+            }
+        }
+
+        while let Some(next) = fast.next.as_ref() {
+            fast = next;
+            slow = slow.next.as_ref().unwrap();
+        }
+        // 采用unsafe来删除呢？
+        let remove = slow.next.as_ref().unwrap() as *const _ as *mut Box<ListNode>;
+        let slow = slow as *const _ as *mut Box<ListNode>;
+        unsafe {
+            let remain = (*remove).next.take();
+            (*slow).next = remain;
+        }
+        head
+    }
+
     //876. 链表的中间结点
     pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
         //一个一次走两步，一个一次走一步，快的到终点时，慢的即中间节点（如果，快的只走一步就到终点，则慢的再走一步）

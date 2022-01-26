acb8ea0e8ba088900b445cc2a988eff5c153e737
tanhao1410 <2528865013@qq.com>
2022-01-26 09:51:40 +0800

    567. 字符串的排列

--- 202201/day0126/src/main.rs
+++ 202201/day0126/src/main.rs
@@ -4,6 +4,49 @@ fn main() {
     println!("Hello, world!");
 }
 
+//567. 字符串的排列
+pub fn check_inclusion(s1: String, s2: String) -> bool {
+    let mut s1_map = vec![0; 26];
+    s1
+        .as_bytes()
+        .iter()
+        .for_each(|&l| s1_map[(l - b'a') as usize] += 1);
+    //寻找一个大小相等的窗口
+    if s2.len() < s1.len() {
+        return false;
+    }
+    let mut s2_map = vec![0; 26];
+    s2
+        .as_bytes()
+        .iter()
+        .take(s1.len())
+        .for_each(|&l| s2_map[(l - b'a') as usize] += 1);
+    let is_equal = |s2_map: &[i32]| {
+        for i in 0..26 {
+            if s2_map[i] != s1_map[i] {
+                return false;
+            }
+        }
+        true
+    };
+    //先判断
+    if is_equal(&s2_map) {
+        return true;
+    }
+    let s2_bytes = s2.as_bytes();
+    for i in s1.len()..s2.len() {
+        //加进来一个，减去一个
+        let add = s2_bytes[i] - b'a';
+        let remove = s2_bytes[i - s1.len()] - b'a';
+        s2_map[add as usize] += 1;
+        s2_map[remove as usize] -= 1;
+        if is_equal(&s2_map) {
+            return true;
+        }
+    }
+    false
+}
+
 //3. 无重复字符的最长子串
 pub fn length_of_longest_substring(s: String) -> i32 {
     //思路：dp[i]以s[i]结尾的最长无重复子串。

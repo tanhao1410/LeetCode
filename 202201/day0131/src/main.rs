eb53df62e5b55614ad9eef5f2d18c893872e075e
tanhao1410 <2528865013@qq.com>
2022-01-31 22:46:50 +0800

    46. 全排列

--- 202201/day0131/src/main.rs
+++ 202201/day0131/src/main.rs
@@ -2,12 +2,38 @@ fn main() {
     println!("Hello, world!");
 }
 
+//46. 全排列
+pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    //采用递归的方式，先选择一个，放在第一个位置，然后，nums中元素少一个，递归实现
+    if nums.len() == 1 {
+        return vec![nums];
+    }
+    let mut res = vec![];
+    for i in 0..nums.len() {
+        //选择一个元素放在最后位置
+        //剩下的元素
+        let mut remain = vec![0; nums.len() - 1];
+        for j in 0..i {
+            remain[j] = nums[j];
+        }
+        for j in i + 1..nums.len() {
+            remain[j - 1] = nums[j]
+        }
+
+        let mut inner_res = permute(remain);
+        inner_res.iter_mut().for_each(|v| v.push(nums[i]));
+        res.append(&mut inner_res);
+    }
+
+    res
+}
+
 //77. 组合
 pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
     //思路：递归，每次选择一个最大的值，n, 那么剩下的只能在n-1中选k - 1个，
-    if k == 1{
+    if k == 1 {
         let mut res = vec![];
-        for i in 1..=n{
+        for i in 1..=n {
             res.push(vec![i]);
         }
         return res;

43cf15a43017b512fc8fe3cdab9786157a9e9d75
tanhao1410 <2528865013@qq.com>
2022-02-02 21:22:37 +0800

    221. 最大正方形

--- 202202/day0202/src/main.rs
+++ 202202/day0202/src/main.rs
@@ -2,6 +2,22 @@ fn main() {
     println!("Hello, world!");
 }
 
+//221. 最大正方形
+pub fn maximal_square(matrix: Vec<Vec<char>>) -> i32 {
+    let mut dp = vec![vec![0; matrix[0].len()]; matrix.len()];
+    for i in 0..dp.len() {
+        for j in 0..dp[0].len() {
+            if matrix[i][j] == '0' {} else if i > 0 && j > 0 {
+                dp[i][j] = 1 + dp[i - 1][j].min(dp[i][j - 1].min(dp[i - 1][j - 1]))
+            } else {
+                dp[i][j] = 1;
+            }
+        }
+    }
+    let max_edge = *dp.iter().flat_map(|v| v.iter()).max().unwrap();
+    max_edge * max_edge
+}
+
 //64. 最小路径和
 pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
     let mut dp = grid.clone();

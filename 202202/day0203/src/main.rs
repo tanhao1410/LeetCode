0ae9ff74b05cfecae0b8c1c1d376a69b1f0b1d61
tanhao1410 <2528865013@qq.com>
2022-02-03 20:59:36 +0800

    136. 只出现一次的数字

--- 202202/day0203/src/main.rs
+++ 202202/day0203/src/main.rs
@@ -2,6 +2,13 @@ fn main() {
     println!("Hello, world!");
 }
 
+//136. 只出现一次的数字
+pub fn single_number(nums: Vec<i32>) -> i32 {
+    nums
+        .iter()
+        .fold(0,|p,&r|p ^ r)
+}
+
 //190. 颠倒二进制位
 pub fn reverse_bits(x: u32) -> u32 {
     let mut bytes = vec![0; 32];

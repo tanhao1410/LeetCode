4c69a4c95449544e173d6ee26cff94f0f60b76a4
tanhao1410 <2528865013@qq.com>
2022-02-04 21:59:43 +0800

    376. 摆动序列

--- 202202/day0204/src/main.rs
+++ 202202/day0204/src/main.rs
@@ -2,6 +2,35 @@ fn main() {
     println!("Hello, world!");
 }
 
+//376. 摆动序列
+pub fn wiggle_max_length(nums: Vec<i32>) -> i32 {
+    //思路：两个dp，一个是nums[i]结尾的 增， 一个以nums[i]结尾的 减
+    let mut dp_inc = nums.clone();
+    let mut dp_dec = nums.clone();
+
+    dp_dec[0] = 1;
+    dp_inc[0] = 1;
+
+    for i in 1..nums.len() {
+        //对于递增的dp来说， nums[i] 如果大于 num[..i]中的一个，那么， dp_inc = dp_dec[i - x] + 1;
+        let mut max_dec = 0;
+        let mut max_inc = 0;
+        for j in 0..i {
+            if nums[j] < nums[i] {
+                max_inc = max_inc.max(dp_dec[j]);
+            } else if nums[j] > nums[i] {
+                max_dec = max_dec.max(dp_inc[j]);
+            }
+        }
+        dp_inc[i] = max_inc + 1;
+        dp_dec[i] = max_dec + 1;
+    }
+
+    dp_dec.into_iter().max().unwrap().max(
+        dp_inc.into_iter().max().unwrap()
+    )
+}
+
 //300. 最长递增子序列
 pub fn length_of_lis(nums: Vec<i32>) -> i32 {
     // o (n ^2)

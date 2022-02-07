329aaf40aecee481b1c99ee2b076f6aa41a83da9
tanhao1410 <2528865013@qq.com>
2022-02-07 21:02:29 +0800

    279. 完全平方数

--- 202202/day0207/src/main.rs
+++ 202202/day0207/src/main.rs
@@ -2,6 +2,35 @@ fn main() {
     println!("Hello, world!");
 }
 
+//279. 完全平方数
+pub fn num_squares(n: i32) -> i32 {
+    //完全平方数有哪些【1,4,9,16,...,10000】共100个。
+    // dp[上述] = 1；
+    //for [上述] 遍历 完全平方数 + 1 dp[]
+    //dp[j] = dp[j - nums[i]] + 1 .min(self);
+    //从小往大的开始算
+    //dp[j + nums[i]] = dp[j] + 1 .min(self)
+    let mut nums = vec![];
+    for i in 1..10000 {
+        if i * i > n {
+            break;
+        }
+        nums.push(i * i);
+    }
+    let mut dp = vec![i32::MAX; n as usize + 1];
+    for i in 0..nums.len() {
+        dp[nums[i] as usize] = 1;
+    }
+    for j in 1..=n {
+        for i in 0..nums.len() {
+            if j + nums[i] <= n {
+                dp[j as usize + nums[i] as usize] = dp[j as usize + nums[i] as usize].min(dp[j as usize] + 1);
+            }
+        }
+    }
+    dp[n as usize]
+}
+
 //377. 组合总和 Ⅳ
 pub fn combination_sum4(nums: Vec<i32>, target: i32) -> i32 {
     // let mut dp = vec![0; target as usize + 1];

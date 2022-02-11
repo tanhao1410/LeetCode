4ab74b6c64cf358b3df4367533e2fb2448e20937
tanhao1410 <2528865013@qq.com>
2022-02-11 20:23:36 +0800

    844. 比较含退格的字符串

--- 202202/day0211/src/main.rs
+++ 202202/day0211/src/main.rs
@@ -2,7 +2,24 @@ fn main() {
     println!("Hello, world!");
 }
 
-
+//844. 比较含退格的字符串
+pub fn backspace_compare(s: String, t: String) -> bool {
+    let process_str = |s: String| -> String{
+        let bytes = s.as_bytes();
+        let mut res_bytes = vec![];
+        for i in 0..bytes.len() {
+            if bytes[i] == b'#' && res_bytes.len() > 0 {
+                if res_bytes.len() > 0 {
+                    res_bytes.remove(res_bytes.len() - 1);
+                }
+            } else {
+                res_bytes.push(bytes[i]);
+            }
+        }
+        String::from_utf8(res_bytes).unwrap()
+    };
+    process_str(s) == process_str(t)
+}
 
 //1984. 学生分数的最小差值
 pub fn minimum_difference(nums: Vec<i32>, k: i32) -> i32 {

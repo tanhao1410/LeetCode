e3caff5d4a518a6e5e16f7b8e9f3abb0116b29d0
tanhao1410 <2528865013@qq.com>
2022-02-15 11:32:37 +0800

    797. 所有可能的路径

--- 202202/day0215/java/Solution.java
+++ 202202/day0215/java/Solution.java
@@ -1,4 +1,29 @@
 class Solution {
+    //797. 所有可能的路径
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        //思路：深度优先递归遍历
+        List<Integer> pre = new ArrayList();
+        pre.add(0);
+        return allPath(graph,pre,0);
+    }
+
+    private List<List<Integer>> allPath(int[][] graph,List<Integer> pre,int start){
+        List<List<Integer>> res = new ArrayList();
+        //从start处开始
+        if (start == graph.length - 1){
+            //即走到了终点
+            res.add(pre);
+            return res;
+        }
+        //还没有走到终点，则看从starr能走到哪
+        for(int dst : graph[start]){
+            List<Integer> new_pre = new ArrayList();
+            new_pre.addAll(pre);
+            new_pre.add(dst);
+            res.addAll(allPath(graph,new_pre,dst));
+        }
+        return res;
+    }
     //547. 省份数量
     public int findCircleNum(int[][] isConnected) {
         int res = 0;

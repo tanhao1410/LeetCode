ea1586021b21a9b8f642326c165ac910759aaf1b
tanhao1410 <2528865013@qq.com>
2022-02-16 17:32:58 +0800

    169. 多数元素

--- 202202/day0216/java/Solution.java
+++ 202202/day0216/java/Solution.java
@@ -1,4 +1,22 @@
 class Solution {
+    //169. 多数元素
+    public int majorityElement(int[] nums) {
+        int res = 0;
+        int count = 0;
+        for(int i = 0;i < nums.length;i ++){
+            if(count == 0){
+                res = nums[i];
+                count ++;
+            }else{
+                if(res == nums[i]){
+                    count ++;
+                }else{
+                    count --;
+                }
+            }
+        }
+        return res;
+    }
 
     //1319. 连通网络的操作次数
     public int makeConnected(int n, int[][] connections) {

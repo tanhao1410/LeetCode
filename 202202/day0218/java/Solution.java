1b3e8170b096ebcb8eaa45b997145f87a69ade02
tanhao1410 <2528865013@qq.com>
2022-02-18 09:41:35 +0800

    1791. 找出星型图的中心节点

--- 202202/day0218/java/Solution.java
+++ 202202/day0218/java/Solution.java
@@ -1,4 +1,40 @@
 class Solution {
+    //1791. 找出星型图的中心节点
+    public int findCenter(int[][] edges) {
+        //广度优先策略。从最外层开始遍历
+        int n = edges.length + 1;
+        boolean[] readed = new boolean[n];
+        LinkedList<Integer> queue = new LinkedList();
+        List<Integer>[] maps = new LinkedList[n];
+        for(int i = 0;i < n;i ++)maps[i] = new LinkedList();
+        for(int[] edge :edges){
+            maps[edge[0] - 1].add(edge[1] - 1);
+            maps[edge[1] - 1].add(edge[1] - 1);
+        }
+        //找到只有一个连接的点，加入到队列中
+        for(int i = 0;i < n;i ++){
+            if (maps[i].size() == 1){
+                queue.add(i);
+                readed[i] =true;
+            }
+        }
+
+        int res = 0;
+        for(int l = queue.size(); l > 0;l = queue.size()){
+            for(int i = 0;i < l;i ++){
+                Integer node = queue.remove(0);
+                res = node;
+                for(Integer j : maps[node]){
+                    if (!readed[j]){
+                        queue.add(j);
+                        readed[j] = true;
+                    }
+                }
+            }
+        }
+        return res + 1;
+    }
+
     //240. 搜索二维矩阵 II
     public boolean searchMatrix(int[][] matrix, int target) {
         //从右上角开始，往左下角开始走，走不动了，返回false

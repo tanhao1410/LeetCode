42e9b0e8402de1cbac2bcfd1f7f5eda93319189f
tanhao1410 <2528865013@qq.com>
2022-02-18 14:08:05 +0800

    365. 水壶问题

--- 202202/day0218/src/main.rs
+++ 202202/day0218/src/main.rs
@@ -5,6 +5,30 @@ fn main() {
 struct Solution;
 
 impl Solution {
+    //365. 水壶问题
+    pub fn can_measure_water(jug1_capacity: i32, jug2_capacity: i32, target_capacity: i32) -> bool {
+        //广度优先策略
+        use std::collections::HashSet;
+        let mut read = HashSet::new();
+        read.insert((0, 0));
+        let mut stack = vec![(0, 0)];
+        while let Some((jug1, jug2)) = stack.pop() {
+            if jug1 == target_capacity || jug2 == target_capacity || jug2 + jug1 == target_capacity {
+                return true;
+            }
+            //8种操作
+            let operators = vec![(jug1_capacity, jug2), (jug1, jug2_capacity), (0, jug2), (jug1, 0),
+                                 (0, jug1 + jug2), (jug1 - jug2_capacity + jug2, jug2_capacity), (jug1 + jug2, 0), (jug1_capacity, jug1 + jug2 - jug1_capacity)];
+            for state in operators {
+                if state.0 >= 0 && state.0 <= jug1_capacity && state.1 >= 0 && state.1 <= jug2_capacity
+                    && !read.contains(&state) {
+                    stack.push(state);
+                    read.insert(state);
+                }
+            }
+        }
+        false
+    }
     //1306. 跳跃游戏 III
     pub fn can_reach(arr: Vec<i32>, start: i32) -> bool {
         //深度优先策略

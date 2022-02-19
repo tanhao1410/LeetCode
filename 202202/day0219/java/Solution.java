87fd77ca0db9cc34f2b44e808ff2882f09d0f068
tanhao1410 <2528865013@qq.com>
2022-02-19 14:49:48 +0800

    969. 煎饼排序

--- 202202/day0219/java/Solution.java
+++ 202202/day0219/java/Solution.java
@@ -1,4 +1,38 @@
 class Solution {
+    //969. 煎饼排序
+    public List<Integer> pancakeSort(int[] arr) {
+        //思路：每一次通过两次翻转，把最大的数放入到合适的位置
+        List<Integer> res = new ArrayList();
+        for (int i = arr.length;i >= 1;i --){
+            if (arr[i - 1] != i){
+                //需要翻转两次，找到i位置，
+                for(int j = 0;j < i;j ++){
+                    if (arr[j] == i){
+                        if(j != 0){
+                            //找到了
+                            res.add(j + 1);
+                            //翻转数组
+                            reverseArr(arr,0,j);
+                        }
+                        res.add(i);
+                        reverseArr(arr,0,i - 1);
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private void reverseArr(int[] arr,int start,int end){
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] =temp;
+            start ++;
+            end --;
+        }
+    }
     //55. 跳跃游戏
     public boolean canJump(int[] nums) {
         //if (nums.length == 1) return true;
--- 202202/day0219/src/main.rs
+++ 202202/day0219/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Solution;
+
+impl Solution {
+    //969. 煎饼排序
+    pub fn pancake_sort(mut arr: Vec<i32>) -> Vec<i32> {
+        let mut res = vec![];
+        let reverse_arr = |arr: &mut Vec<i32>, mut start: usize, mut end: usize| {
+            while start < end {
+                let temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start += 1;
+                end -= 1;
+            }
+        };
+        for i in (1..=arr.len()).rev() {
+            if arr[i - 1] != i as i32 {
+                //需要翻转
+                for j in 0..i {
+                    if arr[j] == i as i32 {
+                        if j != 0 {
+                            //先翻转一次，将这个数翻转到开头
+                            res.push(j as i32 + 1);
+                            reverse_arr(&mut arr, 0, j);
+                        }
+                        res.push(i as i32);
+                        reverse_arr(&mut arr, 0, i - 1);
+                        break;
+                    }
+                }
+            }
+        }
+        res
+    }
+}

fd91559104d68b66c3724803409c0852dc5251ec
tanhao1410 <2528865013@qq.com>
2022-02-20 08:55:07 +0800

    62. 不同路径

--- 202202/day0220/java/Solution.java
+++ 202202/day0220/java/Solution.java
@@ -1,4 +1,22 @@
 class Solution {
+    //62. 不同路径
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0;i < m;i ++){
+            for(int j = 0;j <n ;j ++){
+                if (i > 0 && j > 0){
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }else if (i > 0){
+                    dp[i][j] = dp[i-1][j];
+                }else if (j > 0){
+                    dp[i][j] = dp[i][j-1];
+                }else{
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
     //45. 跳跃游戏 II
     public int jump(int[] nums) {
         //动态规划，广度优先策略

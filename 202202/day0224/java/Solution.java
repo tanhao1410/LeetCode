f657761d080380f622e53c1e11c41d26000e53e8
tanhao1410 <2528865013@qq.com>
2022-02-24 11:13:57 +0800

    2. 两数相加

--- 202202/day0224/java/Solution.java
+++ 202202/day0224/java/Solution.java
@@ -1,4 +1,37 @@
 class Solution {
+    //2. 两数相加
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //用l1返回结果
+        ListNode res = l1;
+        int flag = 0;
+        ListNode pre = null;
+        while(l1 != null && l2 != null){
+            l1.val += l2.val + flag;
+            flag = l1.val/10;
+            l1.val %= 10;
+            pre = l1;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if (l1 == null) {
+            pre.next = l2;
+            l1 = pre.next;
+        }
+
+        while(l1 != null){
+            l1.val += flag;
+            flag = l1.val/10;
+            l1.val %= 10;
+            pre = l1;
+            l1 = l1.next;
+        }
+
+        if (flag > 0){
+            pre.next = new ListNode(flag);
+        }
+
+        return res;
+    }
     //20. 有效的括号
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack();
@@ -16,5 +49,12 @@ class Solution {
             }
         }
         return stack.size() == 0;
+    }
+    class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 }
--- 202202/day0224/src/main.rs
+++ 202202/day0224/src/main.rs
@@ -3,6 +3,52 @@ fn main() {
 }
 
 impl Solution {
+    //2. 两数相加
+    pub fn add_two_numbers(mut l1: Option<Box<ListNode>>, mut l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        //每一次创建一个
+        let mut head = Some(Box::new(ListNode::new(0)));
+        let mut p = head.as_ref().unwrap().as_ref();
+        let mut flag = 0;
+        while l1.is_some() && l2.is_some() {
+            let val = l1.as_ref().unwrap().val + l2.as_ref().unwrap().val + flag;
+            flag = val / 10;
+            let new_node = Some(Box::new(ListNode::new(val % 10)));
+            unsafe {
+                let p_ptr = p as *const ListNode as *mut ListNode;
+                (*p_ptr).next = new_node;
+                p = (*p_ptr).next.as_ref().unwrap().as_ref();
+            }
+            l1 = l1.as_mut().unwrap().next.take();
+            l2 = l2.as_mut().unwrap().next.take();
+        }
+        let mut l3 = None;
+        if l2.is_some() {
+            l3 = l2;
+        }
+        if l1.is_some() {
+            l3 = l1;
+        }
+        while let Some(mut node) = l3 {
+            let val = node.val + flag;
+            flag = val / 10;
+            let new_node = Some(Box::new(ListNode::new(val % 10)));
+            unsafe {
+                let p_ptr = p as *const ListNode as *mut ListNode;
+                (*p_ptr).next = new_node;
+                p = (*p_ptr).next.as_ref().unwrap().as_ref();
+            }
+            l3 = node.next.take();
+        }
+        if flag > 0 {
+            let new_node = Some(Box::new(ListNode::new(flag)));
+            unsafe {
+                let p_ptr = p as *const ListNode as *mut ListNode;
+                (*p_ptr).next = new_node;
+            }
+        }
+        head.as_mut().unwrap().next.take()
+    }
+
     //20. 有效的括号
     pub fn is_valid(s: String) -> bool {
         let mut stack = vec![];
@@ -26,3 +72,19 @@ impl Solution {
 }
 
 struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}

81429f8af179914dae4033b822e41568847ed8f8
tanhao1410 <2528865013@qq.com>
2022-02-25 14:31:56 +0800

    322. 零钱兑换

--- 202202/day0225/java/Solution.java
+++ 202202/day0225/java/Solution.java
@@ -1,4 +1,34 @@
 class Solution {
+    //322. 零钱兑换
+    public int coinChange(int[] coins, int amount) {
+        // dp[i][j] 前i种硬币，能凑出j元的最少硬币数
+        int[][] dp = new int[coins.length][amount + 1];
+        for(int i = 0;i < dp.length;i ++) Arrays.fill(dp[i],-1);
+        for(int i = 0;i < dp.length;i ++){
+            int money = coins[i];
+            for(int j = 0;j <= amount;j ++){
+                //钱是0元时
+                if (j == 0){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                //使用一个当前硬币
+                if (j >= money && dp[i][j - money] != -1){
+                    dp[i][j] = dp[i][j - money] + 1;
+                }
+
+                //不用当前硬币的情况
+                if (i > 0 && dp[i - 1][j] != -1){
+                    if (dp[i][j] != -1){
+                        dp[i][j] = Math.min(dp[i][j],dp[i-1][j]);
+                    }else{
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }
+            }
+        }
+        return dp[coins.length - 1][amount];
+    }
     //537. 复数乘法
     public String complexNumberMultiply(String num1, String num2) {
         StringBuilder res = new StringBuilder();
--- 202202/day0225/src/main.rs
+++ 202202/day0225/src/main.rs
@@ -3,6 +3,24 @@ fn main() {
 }
 
 impl Solution {
+    //322. 零钱兑换
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        let mut dp = vec![vec![-1; amount as usize + 1]; coins.len()];
+        for i in 0..dp.len() {
+            dp[i][0] = 0;
+            for j in 1..dp[0].len() {
+                if j >= coins[i] as usize && dp[i][j - coins[i] as usize] != -1 {
+                    dp[i][j] = dp[i][j - coins[i] as usize] + 1;
+                    if i > 0 && dp[i - 1][j] != -1 && dp[i - 1][j] < dp[i][j] {
+                        dp[i][j] = dp[i - 1][j]
+                    }
+                } else if i > 0 {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        *dp.last().unwrap().last().unwrap()
+    }
     //537. 复数乘法
     pub fn complex_number_multiply(num1: String, num2: String) -> String {
         // a b c d

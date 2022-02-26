348186a9f09fb319fc3c3b726c96b29d65f8c4b3
tanhao1410 <2528865013@qq.com>
2022-02-26 09:01:32 +0800

    2016. 增量元素之间的最大差值

--- 202202/day0226/src/main.rs
+++ 202202/day0226/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+impl Solution {
+    //2016. 增量元素之间的最大差值
+    pub fn maximum_difference(nums: Vec<i32>) -> i32 {
+        let mut max = 0;
+        let mut res = -1;
+        for i in nums.into_iter().rev() {
+            if max > i {
+                res = res.max(max - i);
+            } else {
+                max = i;
+            }
+        }
+        res
+    }
+}
+
+struct Solution;

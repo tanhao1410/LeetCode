26474bd8085af8fac26a6b08d7eb3551196d747b
tanhao1410 <2528865013@qq.com>
2022-03-01 17:49:13 +0800

    968. 监控二叉树

--- 202203/day0301/java/Solution.java
+++ 202203/day0301/java/Solution.java
@@ -1,4 +1,61 @@
 class Solution {
+
+    //968. 监控二叉树
+    public int minCameraCover(TreeNode root) {
+        //TreeNode =>
+        HashMap<TreeNode,Pair[]>  map = new HashMap();
+        return minCameraCover(root,false,false,map);
+
+    }
+    private int minCameraCover(TreeNode root,boolean need,boolean parent,HashMap<TreeNode,Pair[]> map){
+
+        if (root == null){
+            //必须要怎么办？
+            if(need) return 1000;
+            return 0;
+        };
+
+        if(map.containsKey(root)){
+            Pair[] paris = map.get(root);
+            //System.out.println("vv");
+            for(Pair p : paris){
+                if (p.need == need && p.parent == parent){
+                    //
+                    //System.out.println("找到了");
+                    return p.res;
+                }
+            }
+        }
+
+        int res = 1 + minCameraCover(root.left,false,true,map) + minCameraCover(root.right,false,true,map);
+        //不在当前节点布置
+        if (!need){
+            int temp = 0;
+            if(parent){
+                temp = minCameraCover(root.left,false,false,map) + minCameraCover(root.right,false,false,map);
+            }else{
+                temp = Math.min((minCameraCover(root.left,true,false,map) + minCameraCover(root.right,false,false,map)),
+                (minCameraCover(root.left,false,false,map) + minCameraCover(root.right,true,false,map)));
+            }
+            res = Math.min(res,temp);
+        }
+       Pair[] pairs = map.getOrDefault(root,new Pair[4]);
+       for(int i = 0;i < 4;i ++){
+           if (pairs[i] == null) pairs[i] = new Pair(need,parent,res);
+       }
+       map.put(root,pairs);
+        return res;
+    }
+    class Pair{
+        public boolean need;
+        public boolean parent;
+        public int res = 0;
+        public Pair(boolean need,boolean parent,int res){
+            this.need = need;
+            this.parent = parent;
+            this.res = res;
+        }
+    }
     //1042. 不邻接植花
     public int[] gardenNoAdj(int n, int[][] paths) {
         //最多有三个邻居。先将数组paths转换下，转换成一个每个花坛对应的

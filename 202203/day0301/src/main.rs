2c6d03b3fa5ea5939231bb4b7d98c6fc65f6dc06
tanhao1410 <2528865013@qq.com>
2022-03-01 10:58:26 +0800

    105. 从前序与中序遍历序列构造二叉树

--- 202203/day0301/src/main.rs
+++ 202203/day0301/src/main.rs
@@ -3,34 +3,59 @@ fn main() {
 }
 
 impl Solution {
+    //105. 从前序与中序遍历序列构造二叉树
+    pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::build_tree_by_slice(&preorder, &inorder)
+    }
+    fn build_tree_by_slice(preorder: &[i32], inorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>> {
+        //思路：前序 ：中左右。中序 左中右。所有很容易得到哪个是中间
+        //然后递归的方式，分别构造左子树与右子树
+        if preorder.len() == 0 {
+            return None;
+        }
+        //找到根节点
+        let root_val = preorder[0];
+        let mut index = 0;
+        while inorder[index] != root_val {
+            index += 1;
+        }
+        let mut root = TreeNode::new(root_val);
+        if index > 0 {
+            root.left = Self::build_tree_by_slice(&preorder[1..index + 1], &inorder[..index]);
+        }
+        if index < preorder.len() - 1 {
+            root.right = Self::build_tree_by_slice(&preorder[index + 1..], &inorder[index + 1..]);
+        }
+        Some(Rc::new(RefCell::new(root)))
+    }
     //1042. 不邻接植花
     pub fn garden_no_adj(n: i32, paths: Vec<Vec<i32>>) -> Vec<i32> {
         use std::collections::VecDeque;
-        let mut path = vec![vec![];n as usize];
-        for p in &paths{
+        let mut path = vec![vec![]; n as usize];
+        for p in &paths {
             path[p[0] as usize - 1].push(p[1] as usize - 1);
             path[p[1] as usize - 1].push(p[0] as usize - 1);
         }
         let mut queue = VecDeque::new();
-        let mut res = vec![0;n as usize];
-        for i in 0..n as usize{
-            if res[i] == 0{
+        let mut res = vec![0; n as usize];
+        for i in 0..n as usize {
+            if res[i] == 0 {
                 queue.push_back(i);
-                while !queue.is_empty(){
+                while !queue.is_empty() {
                     let len = queue.len();
-                    for _ in 0..len{
+                    for _ in 0..len {
                         let cur = queue.pop_front().unwrap();
-                        let mut temp = vec![0;4];
+                        let mut temp = vec![0; 4];
                         //应该用哪种颜色
-                        for &arround in &path[cur]{
-                            if res[arround] == 0{
+                        for &arround in &path[cur] {
+                            if res[arround] == 0 {
                                 queue.push_back(arround);
-                            }else{
+                            } else {
                                 temp[res[arround] as usize - 1] = 1;
                             }
                         }
-                        for i in 0..4{
-                            if temp[i] == 0{
+                        for i in 0..4 {
+                            if temp[i] == 0 {
                                 res[cur] = i as i32 + 1;
                                 break;
                             }
@@ -110,3 +135,24 @@ impl Solution {
 }
 
 struct Solution;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;

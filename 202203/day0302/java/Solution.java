158814e92bb4360fb2f3d3d470b120c187d74e88
tanhao1410 <2528865013@qq.com>
2022-03-02 10:15:29 +0800

    199. 二叉树的右视图

--- 202203/day0302/java/Solution.java
+++ 202203/day0302/java/Solution.java
@@ -1,4 +1,33 @@
 class Solution {
+    //199. 二叉树的右视图
+    public List<Integer> rightSideView(TreeNode root) {
+        //层次遍历的最后一个。
+        List<Integer> res = new ArrayList();
+        LinkedList<TreeNode> queue = new LinkedList();
+        if(root != null) queue.add(root);
+        while(queue.size() > 0){
+            int size = queue.size();
+            for (int i = 0;i < size;i ++){
+                TreeNode first = queue.remove();
+                if (first.left != null) queue.add(first.left);
+                if (first.right != null) queue.add(first.right);
+                if(i == size - 1) res.add(first.val);
+            }
+        }
+        return res;
+    }
+    public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+    }
     //1886. 判断矩阵经轮转后是否一致
     public boolean findRotation(int[][] mat, int[][] target) {
         int n = mat.length;
--- 202203/day0302/src/main.rs
+++ 202203/day0302/src/main.rs
@@ -1,3 +1,55 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+impl Solution {
+    //199. 二叉树的右视图
+    pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        use std::collections::VecDeque;
+        let mut res = vec![];
+        let mut queue = VecDeque::new();
+        if root.is_some() {
+            queue.push_back(root.as_ref().unwrap().clone());
+        }
+        while !queue.is_empty() {
+            let len = queue.len();
+            for i in 0..len {
+                let cur = queue.pop_front().unwrap();
+                if i == len - 1 {
+                    res.push(cur.borrow().val);
+                }
+                if cur.borrow().left.is_some() {
+                    queue.push_back(cur.borrow().left.as_ref().unwrap().clone());
+                }
+                if cur.borrow().right.is_some() {
+                    queue.push_back(cur.borrow().right.as_ref().unwrap().clone());
+                }
+            }
+        }
+        res
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution;
+
+use std::rc::Rc;
+use std::cell::RefCell;

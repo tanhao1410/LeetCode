1f9dc87e647f999ffdc1a5e640cba4ac54e2b606
tanhao1410 <2528865013@qq.com>
2022-03-03 09:04:30 +0800

    48. 旋转图像

--- 202203/day0303/java/Solution.java
+++ 202203/day0303/java/Solution.java
@@ -1,4 +1,17 @@
 class Solution {
+    //48. 旋转图像
+    public void rotate(int[][] matrix) {
+        for(int i = 0;i < matrix.length/2;i ++){
+            for(int j = 0;j < (matrix.length+1) / 2;j ++){
+                int temp = matrix[i][j];
+                //0,0 <- 2,0
+                matrix[i][j] = matrix[matrix.length - 1 - j][i];
+                matrix[matrix.length - 1 - j][i] = matrix[matrix.length - 1 - i][matrix.length - 1- j];
+                matrix[matrix.length - 1- i][matrix.length - 1 - j] = matrix[j][matrix.length - 1- i];
+                matrix[j][matrix.length - 1 - i] = temp;
+            }
+        }
+    }
     //258. 各位相加
     public int addDigits(int num) {
         if(num <= 9) return num;
--- 202203/day0303/src/main.rs
+++ 202203/day0303/src/main.rs
@@ -3,6 +3,19 @@ fn main() {
 }
 
 impl Solution {
+    //48. 旋转图像
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        let n = matrix.len();
+        for i in 0..n / 2 {
+            for j in 0..(n + 1) / 2 {
+                let temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
     //258. 各位相加
     pub fn add_digits(num: i32) -> i32 {
         if num < 9 {

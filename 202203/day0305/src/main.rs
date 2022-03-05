ae2c0757ee4b63d507451a172ef1e5de086d3307
tanhao1410 <2528865013@qq.com>
2022-03-05 16:56:16 +0800

    42. 接雨水

--- 202203/day0305/src/main.rs
+++ 202203/day0305/src/main.rs
@@ -3,6 +3,28 @@ fn main() {
 }
 
 impl Solution {
+    //42. 接雨水
+    pub fn trap(height: Vec<i32>) -> i32 {
+        //它前面的最大值，它后面的最大值
+        let mut pre_max = vec![0; height.len()];
+        let mut tail_max = vec![0; height.len()];
+        for i in 0..height.len() {
+            if i > 0 {
+                pre_max[i] = pre_max[i - 1].max(height[i]);
+                tail_max[height.len() - 1 - i] = tail_max[height.len() - i].max(height[height.len() - 1 - i]);
+            } else {
+                pre_max[i] = height[i];
+                tail_max[height.len() - 1 - i] = height[height.len() - 1 - i];
+            }
+        }
+        //每一个高度柱能放多少水呢，取决于，它左右两边的最高值的最小值
+        pre_max.into_iter()
+            .zip(tail_max.into_iter())
+            .map(|e| e.0.min(e.1))
+            .zip(height.into_iter())
+            .map(|v| v.0 - v.1)
+            .sum()
+    }
     //503. 下一个更大元素 II
     pub fn next_greater_elements(nums: Vec<i32>) -> Vec<i32> {
         let mut stack = vec![];

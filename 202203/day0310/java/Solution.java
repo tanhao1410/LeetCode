62801472277ca55dae4561687e02bf993806af2f
tanhao1410 <2528865013@qq.com>
2022-03-10 10:21:48 +0800

    138. 复制带随机指针的链表

--- 202203/day0310/java/Solution.java
+++ 202203/day0310/java/Solution.java
@@ -1,4 +1,33 @@
 class Solution {
+    //138. 复制带随机指针的链表
+    public Node copyRandomList(Node head) {
+        //先复制节点，再复制random；用一个hashmap记录。old-new;
+        HashMap<Node,Node> map = new HashMap();
+        Node newHead = null;
+        Node p = null;
+        if(head != null){
+            newHead = new Node(head.val);
+            newHead.random = head.random;
+            map.put(head,newHead);
+            head = head.next;
+            p = newHead;
+        }
+        while (head != null){
+            Node node = new Node(head.val);
+            node.random = head.random;
+            map.put(head,node);
+            head = head.next;
+            p.next = node;
+            p = p.next;
+        }
+        //处理random
+        p = newHead;
+        while (p != null){
+            p.random = map.get(p.random);
+            p = p.next;
+        }
+        return newHead;
+    }
     //589. N 叉树的前序遍历
     public List<Integer> preorder(Node root) {
         //思路：先中再左
@@ -13,6 +42,7 @@ class Solution {
     class Node {
         public int val;
         public List<Node> children;
+        public Node random;
 
         public Node() {}
 

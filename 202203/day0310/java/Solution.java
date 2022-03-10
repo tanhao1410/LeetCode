8b85d28388c30c150c3018669a5bf289bc06e4c8
tanhao1410 <2528865013@qq.com>
2022-03-10 11:08:42 +0800

    143. 重排链表

--- 202203/day0310/java/Solution.java
+++ 202203/day0310/java/Solution.java
@@ -1,4 +1,55 @@
 class Solution {
+
+    //143. 重排链表
+    public void reorderList(ListNode head) {
+        //走到中间，倒置链表，合成插入。
+        ListNode mid = getMid(head);
+        ListNode revMid = reverseListNode(mid);
+        while(revMid != null){
+            ListNode temp = head.next;
+            head.next = revMid;
+            revMid = revMid.next;
+            head.next.next = temp;
+            head = temp;
+        }
+    }
+
+    private ListNode getMid(ListNode head){
+        if (head == null || head.next == null) return null;
+        //双指针走法
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next;
+            if (fast != null){
+                fast = fast.next;
+            }
+            slow = slow.next;
+        }
+        ListNode res = slow.next;
+        slow.next = null;
+        return res;
+    }
+
+    private ListNode reverseListNode(ListNode head){
+        if (head == null || head.next == null) return head;
+        ListNode pre = null;
+        while (head != null){
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     //138. 复制带随机指针的链表
     public Node copyRandomList(Node head) {
         //先复制节点，再复制random；用一个hashmap记录。old-new;

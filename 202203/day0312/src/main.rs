574e5ecacf6fd53eb27b42c8e79799e01293899f
tanhao1410 <2528865013@qq.com>
2022-03-12 14:35:48 +0800

    860. 柠檬水找零

--- 202203/day0312/src/main.rs
+++ 202203/day0312/src/main.rs
@@ -2,6 +2,38 @@ fn main() {
     println!("Hello, world!");
 }
 
+
+impl Solution {
+    //860. 柠檬水找零
+    pub fn lemonade_change(bills: Vec<i32>) -> bool {
+        //优先找零10元的。
+        let mut five = 0;
+        let mut ten = 0;
+        for i in bills {
+            match i {
+                5 => five += 1,
+                10 => {
+                    five -= 1;
+                    ten += 1;
+                }
+                _ => {
+                    //需要找零15元
+                    if ten > 0 {
+                        ten -= 1;
+                        five -= 1;
+                    } else {
+                        five -= 3;
+                    }
+                }
+            }
+            if five < 0 {
+                return false;
+            }
+        }
+        true
+    }
+}
+
 use std::collections::BinaryHeap;
 
 //1845. 座位预约管理系统

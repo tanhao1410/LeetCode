fbaaeaee78e99f9576f4cf668588c481af776ec1
tanhao1410 <2528865013@qq.com>
2022-03-12 14:58:59 +0800

    61. 旋转链表

--- 202203/day0312/src/main.rs
+++ 202203/day0312/src/main.rs
@@ -4,6 +4,43 @@ fn main() {
 
 
 impl Solution {
+    //61. 旋转链表
+    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        if head.is_none() {
+            return None;
+        }
+        let list_len = |mut head: &Option<Box<ListNode>>| {
+            let mut res = 0;
+            while let Some(node) = head {
+                res += 1;
+                head = &node.next;
+            }
+            res
+        };
+        let list_append = |mut head: Option<Box<ListNode>>, tail: Option<Box<ListNode>>| {
+            let mut p = head.as_mut();
+            while let Some(node) = p {
+                if node.next.is_none() {
+                    node.next = tail;
+                    return head;
+                }
+                p = node.next.as_mut();
+            }
+            tail
+        };
+        let len = list_len(&head);
+        let mut k = len - (k % len);
+        if k == 0 {
+            return head;
+        }
+        let mut p = head.as_mut().unwrap();
+        while k > 1 {
+            p = p.next.as_mut().unwrap();
+            k -= 1;
+        }
+        //需要断开
+        list_append(p.next.take(), head)
+    }
     //860. 柠檬水找零
     pub fn lemonade_change(bills: Vec<i32>) -> bool {
         //优先找零10元的。
@@ -31,6 +68,24 @@ impl Solution {
             }
         }
         true
+    }
+}
+
+struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
     }
 }
 

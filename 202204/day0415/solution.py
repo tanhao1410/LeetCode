8aae07fbc3aac2fcf97ef869d225e92eae99bb13
tanhao1410 <2528865013@qq.com>
2022-04-15 15:00:09 +0800

    21. 合并两个有序链表

--- 202204/day0415/solution.py
+++ 202204/day0415/solution.py
@@ -1,9 +1,36 @@
-from typing import List
+from typing import List, Optional
+
 
 class NestedInteger:
     pass
 
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
+
+    # 21. 合并两个有序链表
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        p = head
+        while list2 is not None and list1 is not None:
+            if list1.val > list2.val:
+                p.next = list2
+                list2 = list2.next
+            else:
+                p.next = list1
+                list1 = list1.next
+            p = p.next
+        if list1 is None:
+            p.next = list2
+        else:
+            p.next = list1
+        return head.next
+
     # 385.迷你语法分析器
     def deserialize(self, s: str) -> NestedInteger:
         # 思路：如果是单独的数字，直接生成一个返回即可。

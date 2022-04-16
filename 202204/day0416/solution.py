3e5c7b0145fbe96c985dba541c71edec7966afb8
tanhao1410 <2528865013@qq.com>
2022-04-16 12:14:43 +0800

    479. 最大回文数乘积

---
+++ 202204/day0416/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    # 479. 最大回文数乘积
+    def largestPalindrome(self, n: int) -> int:
+        if n == 1:
+            return 9
+        # 两个n位数相乘的话，得到的结果应该是 2*n - 1 或 2n位
+        # 先看2n位的，然后在看少一位的大小
+        # 98 89 1001 99 999
+        part = 10 ** n - 1
+        for i in range(part, part // 10, -1):
+            # 得到回文数
+            num = i
+            while i != 0:
+                # 得到part的最低位，
+                low = i % 10
+                i //= 10
+                num *= 10
+                num += low
+            # 寻找合适的数
+            j = 10 ** n - 1
+            while j * j >= num:
+                if num % j == 0:
+                    return num % 1337
+                j -= 1
+
+
+for i in range(1, 9):
+    s = Solution()
+    print(s.largestPalindrome(i))

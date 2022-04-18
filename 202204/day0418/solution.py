b60b1b71e459359c0ce515caa2cb2a275338390c
tanhao1410 <2528865013@qq.com>
2022-04-18 16:10:06 +0800

    199. 二叉树的右视图

--- 202204/day0418/solution.py
+++ 202204/day0418/solution.py
@@ -1,8 +1,31 @@
 from typing import List
 
 
+class TreeNode:
+    pass
+
+
 class Solution:
-    #386. 字典序排数
+    #199. 二叉树的右视图
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        # 层次遍历
+        q, res = [], []
+        if root is not None:
+            q.append(root)
+        while len(q) > 0:
+            # 拿出队列中的最后一个
+            qLen = len(q)
+            res.append(q[qLen - 1])
+            for i in range(qLen):
+                first = q[i]
+                if first.left is not None:
+                    q.append(first.left)
+                if first.right is not None:
+                    q.append(first.right)
+            q = q[qLen:]
+        return res
+
+    # 386. 字典序排数
     def lexicalOrder(self, n: int) -> List[int]:
         res = []
         for i in range(1, 10):

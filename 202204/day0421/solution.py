5e7487cdeedaad5aaaa322155e866b23f4892d51
tanhao1410 <2528865013@qq.com>
2022-04-21 05:49:04 +0800

    88. 合并两个有序数组

---
+++ 202204/day0421/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    # 88. 合并两个有序数组
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1, p2, p3 = m - 1, n - 1, m + n - 1
+        while p3 >= 0:
+            if p2 < 0 or p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p3] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p3] = nums2[p2]
+                p2 -= 1
+            p3 -= 1

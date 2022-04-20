9732b9479aa1fdb73f482a6a18904d40d8991c9c
tanhao1410 <2528865013@qq.com>
2022-04-21 06:55:58 +0800

    824. 山羊拉丁文

--- 202204/day0421/solution.py
+++ 202204/day0421/solution.py
@@ -2,6 +2,19 @@ from typing import List
 
 
 class Solution:
+    # 824. 山羊拉丁文
+    def toGoatLatin(self, sentence: str) -> str:
+        letters = {'a', 'e', 'i', 'o', 'u'}
+        words = sentence.split(" ")
+        res = []
+        for index, word in enumerate(words):
+            if word[0].lower() not in letters:
+                word = word[1:] + word[0]
+            word += 'ma'
+            word += 'a' * (index + 1)
+            res.append(word)
+        return ' '.join(res)
+
     # 88. 合并两个有序数组
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """

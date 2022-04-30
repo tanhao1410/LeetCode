0da392ad0e045e93fabe2ce2627f0a054c2b8a28
tanhao1410 <2528865013@qq.com>
2022-04-30 16:39:18 +0800

    812. 最大三角形面积

--- 202204/day0430/__init__.py
+++ 202204/day0430/__init__.py
@@ -2,6 +2,19 @@ from typing import List
 
 
 class Solution:
+    # 812. 最大三角形面积
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        def triangleArea(p1, p2, p3) -> float:
+            return 0.5 * abs(
+                p1[0] * p2[1] + p2[0] * p3[1] + p3[0] * p1[1] - p1[1] * p2[0] - p2[1] * p3[0] - p3[1] * p1[0])
+
+        res = 0
+        for i in range(len(points) - 2):
+            for j in range(i + 1, len(points) - 1):
+                for k in range(j + 1, len(points)):
+                    res = max(res, triangleArea(points[i], points[j], points[k]))
+        return res
+
     # 908. 最小差值 I
     def smallestRangeI(self, nums: List[int], k: int) -> int:
         # return max(0,max(nums) - min(nums) - 2 * k)

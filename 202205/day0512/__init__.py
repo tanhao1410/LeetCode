3955da75e3307efd1174f2a482b0951a53f58b76
tanhao1410 <2528865013@qq.com>
2022-05-12 15:54:32 +0800

    942. 增减字符串匹配

--- 202205/day0512/__init__.py
+++ 202205/day0512/__init__.py
@@ -2,6 +2,20 @@ from typing import List
 
 
 class Solution:
+    # 942. 增减字符串匹配
+    def diStringMatch(self, s: str) -> List[int]:
+        big, small = len(s), 0
+        res = []
+        for i in reversed(s):
+            if i == 'D':
+                res.append(small)
+                small += 1
+            else:
+                res.append(big)
+                big -= 1
+        res.append(big)
+        return list(reversed(res))
+
     # 806. 写字符串需要的行数
     def numberOfLines(self, widths: List[int], s: str) -> List[int]:
         row, rem = 1, 0

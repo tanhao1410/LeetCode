e415708006a7c0c39338bbb00b371cf873d8c745
tanhao1410 <2528865013@qq.com>
2022-05-12 15:45:55 +0800

    806. 写字符串需要的行数

--- 202205/day0512/__init__.py
+++ 202205/day0512/__init__.py
@@ -2,6 +2,20 @@ from typing import List
 
 
 class Solution:
+    # 806. 写字符串需要的行数
+    def numberOfLines(self, widths: List[int], s: str) -> List[int]:
+        row, rem = 1, 0
+        for i in s:
+            # 确认i占多少位置
+            width = widths[ord(i) - ord('a')]
+            # 剩余多少宽度 100 - rem
+            if width > 100 - rem:
+                row += 1
+                rem = width
+            else:
+                rem += width
+        return [row, rem]
+
     # 944. 删列造序
     def minDeletionSize(self, strs: List[str]) -> int:
         res = 0

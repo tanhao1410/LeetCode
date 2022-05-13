5d1b56eac194d0eb35db662fca9ec516c81dd0b5
tanhao1410 <2528865013@qq.com>
2022-05-13 13:26:21 +0800

    991. 坏了的计算器

--- 202205/day0513/__init__.py
+++ 202205/day0513/__init__.py
@@ -1,4 +1,36 @@
 class Solution:
+    # 991. 坏了的计算器
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        # 广度遍历+剪枝
+        # 如果target>start,则所有小于start的都去除掉。如果target比目标小呢，直接返回即可。
+        # 如果当前值的2倍小于目标值，则只需要加入2倍的，而不用加入-1的
+        # if target <= startValue:
+        #     return startValue - target
+        # reached = {startValue}
+        # q = deque(reached)
+        # res = 0
+        # while True:
+        #     q_len = len(q)
+        #     for _ in range(q_len):
+        #         num = q.popleft()
+        #         if num == target:
+        #             return res
+        #         if num * 2 not in reached:
+        #             q.append(num * 2)
+        #             reached.add(num * 2)
+        #         if num * 2 > target and num - 1 > startValue and num - 1 not in reached:
+        #             q.append(num - 1)
+        #             reached.add(num - 1)
+        #     res += 1
+        res = 0
+        while target > startValue:
+            res += 1
+            if target % 2 == 1:
+                target += 1
+            else:
+                target //= 2
+        return res + startValue - target
+
     # 984. 不含 AAA 或 BBB 的字符串
     def strWithout3a3b(self, a: int, b: int) -> str:
         res = ''

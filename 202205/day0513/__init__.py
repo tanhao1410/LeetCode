1c05db23c3a21f0a38e4dd4424d767606b21fa87
tanhao1410 <2528865013@qq.com>
2022-05-13 08:51:43 +0800

    984. 不含 AAA 或 BBB 的字符串

--- 202205/day0513/__init__.py
+++ 202205/day0513/__init__.py
@@ -1,4 +1,25 @@
 class Solution:
+    # 984. 不含 AAA 或 BBB 的字符串
+    def strWithout3a3b(self, a: int, b: int) -> str:
+        res = ''
+        # 思路：每一次追加一个字母，追加谁呢，剩余的ab谁多加谁，而且不能出现连着三个。
+        while a > 0 or b > 0:
+            if a > b:
+                if len(res) > 1 and res[-1] == res[-2] and res[-1] == 'a':
+                    res += 'b'
+                    b -= 1
+                else:
+                    res += 'a'
+                    a -= 1
+            else:
+                if len(res) > 1 and res[-1] == res[-2] and res[-1] == 'b':
+                    res += 'a'
+                    a -= 1
+                else:
+                    res += 'b'
+                    b -= 1
+        return res
+
     # 面试题 01.05. 一次编辑
     def oneEditAway(self, first: str, second: str) -> bool:
         def moreThanOne(first: str, second: str) -> bool:

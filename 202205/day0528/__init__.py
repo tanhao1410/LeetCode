61ebe39a9e7c5207a85e7963211fb52af787c714
tanhao1410 <2528865013@qq.com>
2022-05-28 11:19:27 +0800

    1047. 删除字符串中的所有相邻重复项

--- 202205/day0528/__init__.py
+++ 202205/day0528/__init__.py
@@ -1,4 +1,23 @@
 class Solution:
+    # 1047. 删除字符串中的所有相邻重复项
+    def removeDuplicates(self, s: str) -> str:
+        # 怎么删除呢？多次遍历的情况
+        flag = False
+        res = ''
+        for i in range(len(s)):
+            if res == '':
+                res += s[i]
+            else:
+                if s[i] == res[-1]:
+                    flag = True
+                    res = res[:-1]
+                else:
+                    res += s[i]
+        if flag:
+            return self.removeDuplicates(res)
+        else:
+            return res
+
     # 1021. 删除最外层的括号
     def removeOuterParentheses(self, s: str) -> str:
         # 思路：通过一个变量来记录左括号的数量，一旦左括号为0的话，说明前面切出来的为一个 符合逻辑的括号，将它交由单个括号处理函数处理
--- 202205/day0528/src/main.rs
+++ 202205/day0528/src/main.rs
@@ -5,6 +5,27 @@ fn main() {
 struct Solution;
 
 impl Solution {
+    //1047. 删除字符串中的所有相邻重复项
+    pub fn remove_duplicates(s: String) -> String {
+        if s.len() == 0 {
+            return s;
+        }
+        let bytes = s.as_bytes();
+        let mut res = vec![bytes[0]];
+        let mut flag = false;
+        for i in bytes.into_iter().skip(1) {
+            if i == res.last().unwrap_or(&b'*') {
+                res.remove(res.len() - 1);
+                flag = true;
+            } else {
+                res.push(*i);
+            }
+        }
+        if flag {
+            return Self::remove_duplicates(String::from_utf8(res).unwrap());
+        }
+        String::from_utf8(res).unwrap()
+    }
     //1021. 删除最外层的括号
     pub fn remove_outer_parentheses(s: String) -> String {
         let mut left_count = 0;

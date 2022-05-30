033f728ea9504dca483ea01d215b8546421b3d1a
tanhao1410 <2528865013@qq.com>
2022-05-30 15:33:26 +0800

    655. 输出二叉树

--- 202205/day0530/__init__.py
+++ 202205/day0530/__init__.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 
 class TreeNode:
@@ -9,6 +9,34 @@ class TreeNode:
 
 
 class Solution:
+    # 655. 输出二叉树
+    def printTree(self, root: TreeNode, height=0) -> List[List[str]]:
+        m = max(self.treeHeight(root), height)
+        if m == 0:
+            return [[]]
+        n = 2 ** m - 1
+        # 递归式解决方案，对于它的左边来说，对于它的右边来说。
+        # 只关心自己这一层而已。下一层怎么解决呢？
+        # 如果自己本身是空怎么办？
+        res = [['' for _ in range(n)]]
+        if root:
+            res[0][n // 2] = str(root.val)
+            left_res = self.printTree(root.left, m - 1)
+            right_res = self.printTree(root.right, m - 1)
+        else:
+            left_res = self.printTree(None, m - 1)
+            right_res = self.printTree(None, m - 1)
+        for i in range(1, m):
+            if left_res[i - 1]:
+                left_res[i - 1].append('')
+                res.append(left_res[i - 1] + right_res[i - 1])
+        return res
+
+    def treeHeight(self, root: TreeNode) -> int:
+        if root:
+            return max(self.treeHeight(root.left), self.treeHeight(root.right)) + 1
+        return 0
+
     # 1022. 从根到叶的二进制数之和
     def sumRootToLeaf(self, root: Optional[TreeNode], pre_value=0) -> int:
         pre_value = pre_value * 2 + root.val
--- 202205/day0530/src/main.rs
+++ 202205/day0530/src/main.rs
@@ -4,6 +4,43 @@ fn main() {
 
 
 impl Solution {
+    //655. 输出二叉树
+    pub fn print_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<String>> {
+        Self::print_tree_height(&root, 0)
+    }
+
+    fn print_tree_height(root: &Option<Rc<RefCell<TreeNode>>>, height: usize) -> Vec<Vec<String>> {
+        let m = height.max(Self::tree_height(root));
+        if m == 0 {
+            return vec![];
+        }
+        let n = 2usize.pow(m as u32) - 1;
+        let mut res = vec![vec!["".to_string(); n]];
+        if root.is_some() {
+            res[0][n / 2] = root.as_ref().unwrap().borrow().val.to_string();
+        }
+        let default_tree = Rc::new(RefCell::new(TreeNode::new(0)));
+        let left_res = Self::print_tree_height(&root.as_ref().unwrap_or(&default_tree).borrow().left, m - 1);
+        let right_res = Self::print_tree_height(&root.as_ref().unwrap_or(&default_tree).borrow().right, m - 1);
+        for i in 1..m {
+            let mut row = left_res[i - 1].clone();
+            row.push("".to_string());
+            row.append(&mut right_res[i - 1].clone());
+            res.push(row);
+        }
+        res
+    }
+
+
+    fn tree_height(root: &Option<Rc<RefCell<TreeNode>>>) -> usize {
+        let mut res = 0;
+        if let Some(node) = root {
+            res += Self::tree_height(&node.borrow().left).max(
+                Self::tree_height(&node.borrow().right)
+            ) + 1
+        }
+        res
+    }
     //1022. 从根到叶的二进制数之和
     pub fn sum_root_to_leaf(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         Self::sum_root_to_leaf_pre(root.as_ref().unwrap(), 0)

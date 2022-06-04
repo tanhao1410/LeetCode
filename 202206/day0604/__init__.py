42b59f0b2954c0e27b0320cd13f51b2afee71227
tanhao1410 <2528865013@qq.com>
2022-06-04 10:45:04 +0800

    1572. 矩阵对角线元素的和

--- 202206/day0604/__init__.py
+++ 202206/day0604/__init__.py
@@ -2,6 +2,13 @@ from typing import List
 
 
 class Solution:
+    # 1572. 矩阵对角线元素的和
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        res = sum([mat[i][i] + mat[i][len(mat) - 1 - i] for i in range(len(mat))])
+        if len(mat) % 2 == 1:
+            res -= mat[len(mat) // 2][len(mat) // 2]
+        return res
+
     # 929. 独特的电子邮件地址
     def numUniqueEmails(self, emails: List[str]) -> int:
         email_set = set()
--- 202206/day0604/src/main.rs
+++ 202206/day0604/src/main.rs
@@ -5,6 +5,15 @@ fn main() {
 struct Solution;
 
 impl Solution {
+    //1572. 矩阵对角线元素的和
+    pub fn diagonal_sum(mat: Vec<Vec<i32>>) -> i32 {
+        let n = mat.len();
+        let res = (0..n).map(|i| mat[i][i] + mat[i][n - 1 - i]).sum::<i32>();
+        match n % 2 == 0 {
+            true => res,
+            _ => res - mat[n / 2][n / 2]
+        }
+    }
     //929. 独特的电子邮件地址
     pub fn num_unique_emails(emails: Vec<String>) -> i32 {
         use std::collections::HashSet;

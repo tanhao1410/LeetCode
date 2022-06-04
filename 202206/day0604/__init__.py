6073493dea2c3eff9e8a1e7115ce2a7781fedf44
tanhao1410 <2528865013@qq.com>
2022-06-04 14:02:12 +0800

    1544. 整理字符串

--- 202206/day0604/__init__.py
+++ 202206/day0604/__init__.py
@@ -2,6 +2,13 @@ from typing import List
 
 
 class Solution:
+    # 1544. 整理字符串
+    def makeGood(self, s: str) -> str:
+        for i in range(len(s) - 1):
+            if abs(ord(s[i]) - ord(s[i + 1])) == abs(ord('a') - ord('A')):
+                return self.makeGood(s[:i] + s[i + 2:])
+        return s
+
     # 1572. 矩阵对角线元素的和
     def diagonalSum(self, mat: List[List[int]]) -> int:
         res = sum([mat[i][i] + mat[i][len(mat) - 1 - i] for i in range(len(mat))])

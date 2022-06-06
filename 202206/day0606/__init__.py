21af0ddfb98bf540f3ac193f0d72011cab833f17
tanhao1410 <2528865013@qq.com>
2022-06-06 18:14:14 +0800

    2094. 找出 3 位偶数

--- 202206/day0606/__init__.py
+++ 202206/day0606/__init__.py
@@ -2,6 +2,25 @@ from typing import List
 
 
 class Solution:
+    # 2094. 找出 3 位偶数
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        count = [0 for _ in range(10)]
+        for i in digits:
+            count[i] += 1
+
+        def can_create_num(num):
+            a, b, c = num % 10, num // 10 % 10, num // 100 % 10
+            count[a] -= 1
+            count[b] -= 1
+            count[c] -= 1
+            res = count[a] >= 0 and count[b] >= 0 and count[c] >= 0
+            count[a] += 1
+            count[b] += 1
+            count[c] += 1
+            return res
+
+        return list(filter(can_create_num, range(100, 999, 2)))
+
     # LCP 01. 猜数字
     def game(self, guess: List[int], answer: List[int]) -> int:
         res = 0
--- 202206/day0606/src/main.rs
+++ 202206/day0606/src/main.rs
@@ -2,6 +2,31 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct Solution;
+
+impl Solution {
+    //2094. 找出 3 位偶数
+    pub fn find_even_numbers(digits: Vec<i32>) -> Vec<i32> {
+        let counts = digits.into_iter().fold(vec![0; 10], |mut counts, n| {
+            counts[n as usize] += 1;
+            counts
+        });
+        let mut contains = |mut num: i32| {
+            let mut num_counts = vec![0; 10];
+            while num > 0 {
+                num_counts[num as usize % 10] += 1;
+                num /= 10;
+            }
+            num_counts.iter().zip(counts.iter()).all(|e| e.0 <= e.1)
+        };
+
+        (100..999)
+            .step_by(2)
+            .filter(|n| contains(*n))
+            .collect()
+    }
+}
+
 use std::collections::BTreeMap;
 
 //732. 我的日程安排表 III

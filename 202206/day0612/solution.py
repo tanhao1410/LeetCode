8d3808170c71ed86788f142606fa9a22838fa43e
tanhao1410 <2528865013@qq.com>
2022-06-12 12:01:51 +0800

    1145. 二叉树着色游戏

--- 202206/day0612/solution.py
+++ 202206/day0612/solution.py
@@ -9,6 +9,46 @@ class TreeNode:
 
 
 class Solution:
+    # 1145. 二叉树着色游戏
+    def btreeGameWinningMove(self, root: Optional[TreeNode], n: int, x: int) -> bool:
+        # 1.选择x的父节点
+        if root.val != x:
+            # 求某一个节点下的所有节点数
+            x_count = self.btree_node_count(root, x)
+            if x_count * 2 < n:
+                return True
+        # 2.求子节点
+        child_max = self.btree_child_node_max(root, x)
+        if child_max * 2 > n:
+            return True
+        return False
+
+    def btree_child_node_max(self, root: Optional[TreeNode], x: int):
+        res = 0
+        if root:
+            if root.val == x:
+                left = self.btree_node_count(root.left, contains=True)
+                right = self.btree_node_count(root.right, contains=True)
+                res = max(left, right)
+            else:
+                left = self.btree_child_node_max(root.left, x)
+                right = self.btree_child_node_max(root.right, x)
+                res = max(left, right)
+        return res
+
+    def btree_node_count(self, root: Optional[TreeNode], x=0, contains=False):
+        res = 0
+        if root:
+            if contains or root.val == x:
+                res += 1
+                res += self.btree_node_count(root.left, contains=True)
+                res += self.btree_node_count(root.right, contains=True)
+            else:
+                left = self.btree_node_count(root.left, x)
+                right = self.btree_node_count(root.right, x)
+                res = max(left, right)
+        return res
+
     # 1008. 前序遍历构造二叉搜索树
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
         # 对preorder进行切分
--- 202206/day0612/src/main.rs
+++ 202206/day0612/src/main.rs
@@ -5,6 +5,87 @@ fn main() {
 struct Solution;
 
 impl Solution {
+    //1145. 二叉树着色游戏
+    pub fn btree_game_winning_move(root: Option<Rc<RefCell<TreeNode>>>, n: i32, x: i32) -> bool {
+        //1.选择x的父节点，则除了x及其子节点外，其余的都是y，若大于，则可以赢
+        let root_ref = root.as_ref().unwrap().borrow();
+        if root_ref.val != x {
+            //不是跟节点
+            let x_num = Self::btee_node_num(&root, x);
+            //可以选择x节点的父节点。
+            if x_num * 2 < n as usize {
+                return true;
+            }
+        }
+        //2.选x的子节点，则该子节点总和 大于剩余的其它的所有 可以赢。
+        let child = Self::btree_child(&root, x);
+        if let Some(left) = child.0 {
+            let left_num = Self::btee_node_num(&root, left);
+            println!("{}", left_num);
+            if left_num * 2 > n as usize {
+                return true;
+            }
+        }
+
+        if let Some(right) = child.1 {
+            let right = Self::btee_node_num(&root, right);
+            if right * 2 > n as usize {
+                return true;
+            }
+        }
+
+        //3.其余的地方都不应选择，或者说都不是最优选择。
+        false
+    }
+
+    fn btree_child(root: &Option<Rc<RefCell<TreeNode>>>, n: i32) -> (Option<i32>, Option<i32>) {
+        let mut res = (None, None);
+        if let Some(root) = root.as_ref() {
+            let root_ref = root.borrow();
+            if root_ref.val == n {
+                if root_ref.left.is_some() {
+                    res.0 = Some(root_ref.left.as_ref().unwrap().borrow().val);
+                }
+                if root_ref.right.is_some() {
+                    res.1 = Some(root_ref.right.as_ref().unwrap().borrow().val);
+                }
+                return res;
+            }
+            let left = Self::btree_child(&root_ref.left, n);
+            let right = Self::btree_child(&root_ref.right, n);
+            if left.0.is_some() || left.1.is_some() {
+                return left;
+            }
+            return right;
+        }
+        res
+    }
+
+    fn btee_node_num(root: &Option<Rc<RefCell<TreeNode>>>, n: i32) -> usize {
+        let mut res = 0;
+        if let Some(root) = root {
+            let root_ref = root.borrow();
+            if root_ref.val == n {
+                return Self::btree_num(root.clone());
+            }
+            let left = Self::btee_node_num(&root_ref.left, n);
+            let right = Self::btee_node_num(&root_ref.right, n);
+            res = left.max(right);
+        }
+        res
+    }
+
+    fn btree_num(root: Rc<RefCell<TreeNode>>) -> usize {
+        let mut res = 1;
+        if root.borrow().left.is_some() {
+            res += Self::btree_num(root.borrow().left.as_ref().unwrap().clone());
+        }
+        if root.borrow().right.is_some() {
+            res += Self::btree_num(root.borrow().right.as_ref().unwrap().clone());
+        }
+        res
+    }
+
     //1008. 前序遍历构造二叉搜索树
     pub fn bst_from_preorder<T: AsRef<[i32]>>(preorder: T) -> Option<Rc<RefCell<TreeNode>>> {
         let preorder = preorder.as_ref();

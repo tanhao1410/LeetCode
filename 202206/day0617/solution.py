96a106ca55099e0115c08050c101190b28ff6d8d
tanhao1410 <2528865013@qq.com>
2022-06-17 10:38:44 +0800

    1577 数的平方等于两数乘积的方法数

--- 202206/day0617/solution.py
+++ 202206/day0617/solution.py
@@ -1,4 +1,22 @@
+from collections import Counter
+from typing import List
 
 
+class Solution:
+    # 1577 数的平方等于两数乘积的方法数
+    def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        return self.numTriplets2(nums2, nums1) + self.numTriplets2(nums1, nums2)
 
-
+    def numTriplets2(self, nums1: List[int], nums2: List[int]) -> int:
+        nums2_counter = Counter(nums2)
+        res = 0
+        for num in nums1:
+            num = num * num
+            for num2 in nums2_counter.keys():
+                if num % num2 == 0 and num / num2 in nums2_counter.keys():
+                    if num2 == num / num2:
+                        n = nums2_counter[num2]
+                        res += n * (n - 1) // 2
+                    elif num2 > num / num2:
+                        res += nums2_counter[num2] * nums2_counter[num / num2]
+        return res
--- 202206/day0617/src/main.rs
+++ 202206/day0617/src/main.rs
@@ -5,6 +5,35 @@ fn main() {
 struct Solution;
 
 impl Solution {
+    //1577 数的平方等于两数乘积的方法数
+    pub fn num_triplets(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        let triplets = |nums1: &[i32], nums2: &[i32]| -> i32{
+            use std::collections::HashMap;
+            let mut map: HashMap<i32, i32> = HashMap::new();
+            for num in nums2 {
+                let entry = map.entry(*num).or_insert(0);
+                *entry += 1;
+            }
+            let mut res = 0;
+            for num in nums1 {
+                //数字过大怎么办？
+                let num = *num as i64 * *num as i64;
+                for (k, v) in map.iter() {
+                    if (num % *k as i64) == 0 && map.contains_key(&((num / *k as i64) as i32)) {
+                        if num / *k as i64 == *k as i64 {
+                            res += v * (v - 1) / 2;
+                        } else if *k as i64 > num / *k as i64 {
+                            res += v * map.get(&((num / *k as i64) as i32)).unwrap();
+                        }
+                    }
+                }
+            }
+            res
+        };
+
+        triplets(&nums2, &nums1) + triplets(&nums1, &nums2)
+    }
+
     //1089. 复写零
     pub fn duplicate_zeros(arr: &mut Vec<i32>) {
         let arr2 = arr.clone();

db282300e383e5f86d4c710ed77d222e7ce0dda8
tanhao1410 <2528865013@qq.com>
2022-06-22 10:01:25 +0800

    1315. 祖父节点值为偶数的节点和

--- 202206/day0622/solution.py
+++ 202206/day0622/solution.py
@@ -10,6 +10,33 @@ class TreeNode:
 
 
 class Solution:
+    # 1315. 祖父节点值为偶数的节点和
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        # 思路：递归法：一个节点是偶数的话，那么它的所有孙节点应该加入结果，如果不是，答案为其子节点
+        res = 0
+        if root:
+            if root.val % 2 == 0:
+                res += self.sumGrandSon(root)
+            res += self.sumEvenGrandparent(root.left)
+            res += self.sumEvenGrandparent(root.right)
+        return res
+
+    def sumGrandSon(self, root: TreeNode) -> int:
+        res = 0
+        if root.left:
+            res += self.sumSon(root.left)
+        if root.right:
+            res += self.sumSon(root.right)
+        return res
+
+    def sumSon(self, root: TreeNode) -> int:
+        res = 0
+        if root.left:
+            res += root.left.val
+        if root.right:
+            res += root.right.val
+        return res
+
     # 513. 找树左下角的值
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
         queue = deque()
--- 202206/day0622/src/main.rs
+++ 202206/day0622/src/main.rs
@@ -3,6 +3,40 @@ fn main() {
 }
 
 impl Solution {
+    //1315. 祖父节点值为偶数的节点和
+    pub fn sum_even_grandparent(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let sum_son = |root: Rc<RefCell<TreeNode>>| {
+            let mut res = 0;
+            let node_ref = root.borrow();
+            res += node_ref.left.as_ref().map_or(0, |e| e.borrow().val);
+            res += node_ref.right.as_ref().map_or(0, |e| e.borrow().val);
+            res
+        };
+
+        let sum_grand_son = |root: Rc<RefCell<TreeNode>>| {
+            let mut res = 0;
+            let node_ref = root.borrow();
+            if node_ref.left.is_some() {
+                res += sum_son(node_ref.left.as_ref().unwrap().clone());
+            }
+            if node_ref.right.is_some() {
+                res += sum_son(node_ref.right.as_ref().unwrap().clone());
+            }
+            res
+        };
+
+        let mut res = 0;
+        if let Some(node) = root {
+            if node.borrow().val % 2 == 0 {
+                res += sum_grand_son(node.clone());
+            }
+
+            res += Self::sum_even_grandparent(node.borrow_mut().left.take());
+            res += Self::sum_even_grandparent(node.borrow_mut().right.take());
+        }
+        res
+    }
+
     //513. 找树左下角的值
     pub fn find_bottom_left_value(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
         let mut queue = std::collections::VecDeque::new();

a8dd44424005f3839bf9406e940c3ec0ec260c96
tanhao1410 <2528865013@qq.com>
2022-06-23 11:03:33 +0800

    1146. 快照数组

--- 202206/day0623/solution.py
+++ 202206/day0623/solution.py
@@ -13,3 +13,31 @@ class Solution:
             res = self.get_count(num * 3 + 1, map_count)
         map_count[num] = res + 1
         return map_count[num]
+
+
+# 1146. 快照数组
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.datas = {}
+        self.snap_num = 0
+
+    def set(self, index: int, val: int) -> None:
+        values = self.datas.get(index, [])
+        if len(values) > 0 and values[-1][0] == self.snap_num:
+            values[-1] = (self.snap_num, val)
+        else:
+            values.append((self.snap_num, val))
+        self.datas[index] = values
+
+    def snap(self) -> int:
+        self.snap_num += 1
+        return self.snap_num - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        if index in self.datas:
+            values = self.datas[index]
+            for i in range(len(values) - 1, -1, -1):
+                if values[i][0] <= snap_id:
+                    return values[i][1]
+        return 0
--- 202206/day0623/src/main.rs
+++ 202206/day0623/src/main.rs
@@ -27,3 +27,45 @@ impl Solution {
         nums[k as usize - 1]
     }
 }
+
+use std::collections::HashMap;
+
+//1146. 快照数组
+struct SnapshotArray {
+    snap_num: i32,
+    datas: HashMap<i32, Vec<(i32, i32)>>,
+}
+
+impl SnapshotArray {
+    fn new(length: i32) -> Self {
+        Self { datas: HashMap::new(), snap_num: 0 }
+    }
+
+    fn set(&mut self, index: i32, val: i32) {
+        let entry = self.datas.entry(index).or_insert(vec![]);
+        //当前的snap中是否已经存了值了，如果存了，更新，没存，加入一个值。
+        if let Some((snap_id, v)) = entry.last_mut() {
+            if *snap_id == self.snap_num {
+                *v = val;
+                return;
+            }
+        }
+        entry.push((self.snap_num, val));
+    }
+    fn snap(&mut self) -> i32 {
+        self.snap_num += 1;
+        self.snap_num - 1
+    }
+
+    fn get(&self, index: i32, snap_id: i32) -> i32 {
+        self.datas
+            .get(&index)
+            .map_or(0, |v| v
+                .into_iter()
+                .take_while(|e| e.0 <= snap_id)
+                .last()
+                .unwrap_or(&(0, 0))
+                .1,
+            )
+    }
+}
